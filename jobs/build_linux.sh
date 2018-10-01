d713bd492a27437878b08ea2d19c48f3f984a8e2
Markus Cozowicz <marcozo@microsoft.com>
2018-10-01 17:09:48 +0100

    trim linux build

--- jobs/build_linux.sh
+++ jobs/build_linux.sh
@@ -2,27 +2,21 @@
 wget -q https://repo.continuum.io/miniconda/Miniconda3-3.7.0-Linux-x86_64.sh -O ~/miniconda.sh  
 bash ~/miniconda.sh -b -p $HOME/miniconda
 
-echo b.1
 conda create -n xDeepFM-criteo python=3.6
-echo b.2
  
 # source $HOME/miniconda/bin/activate
 export PATH="$HOME/miniconda/envs/xDeepFM-criteo/bin:$PATH"
 echo $PATH
-
-echo d.1 
-ls -l $HOME/miniconda/envs/xDeepFM-criteo/bin
-echo d.2 
 
 # source $HOME/miniconda/bin/activate
 # export PATH="$HOME/miniconda/bin:$PATH"
 # echo $PATH
 cd examples/notebooks/xDeepFM
 
-echo 'debug.3'
-conda env update -f environment.yml
-source activate xDeepFM-criteo
+# echo 'debug.3'
+# conda env update -f environment.yml
+# source activate xDeepFM-criteo
 
-bash ./run_notebooks.sh
+# bash ./run_notebooks.sh
 
 
