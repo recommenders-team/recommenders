3622e237c081fef52b1f180d5712c00bfe85c57e
Markus Cozowicz <marcozo@microsoft.com>
2018-10-01 14:04:52 +0100

    added missing files

--- jobs/build_linux.sh
+++ jobs/build_linux.sh
@@ -2,12 +2,16 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-3.7.0-Linux-x86_64.sh -O ~/miniconda.sh  
 bash ~/miniconda.sh -b -p $HOME/miniconda
 
-export PATH="$HOME/miniconda/bin:$PATH"
+echo 'debug.1'
 source $HOME/miniconda/bin/activate
+echo 'debug.2'
+
+export PATH="$HOME/miniconda/bin:$PATH"
 
 echo $PATH
 cd examples/notebooks/xDeepFM
 
+echo 'debug.3'
 conda env create -f environment.yml
 source activate xDeepFM-criteo
 
