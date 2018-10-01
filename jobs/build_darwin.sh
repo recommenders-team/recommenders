8a254df7e6a36bc01ff026fe821bc6b2e9643603
Markus Cozowicz <marcozo@microsoft.com>
2018-10-01 14:44:58 +0100

    try to fix linux build

--- jobs/build_darwin.sh
+++ jobs/build_darwin.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh  
+wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh  
 bash ~/miniconda.sh -b -p $HOME/miniconda
 
 export PATH="$HOME/miniconda/bin:$PATH"
 source $HOME/miniconda/bin/activate
 
-echo $PATH
 cd examples/notebooks/xDeepFM 
 
-pwd
-
-echo 'creating environment.1'
 conda env create -f environment.yml
-echo 'creating environment.2'
-
-conda info --envs
-
-echo 'trying to activate'
 
 conda activate xDeepFM-criteo
 
--- jobs/build_linux.sh
+++ jobs/build_linux.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-wget https://repo.continuum.io/miniconda/Miniconda3-3.7.0-Linux-x86_64.sh -O ~/miniconda.sh  
+wget -q https://repo.continuum.io/miniconda/Miniconda3-3.7.0-Linux-x86_64.sh -O ~/miniconda.sh  
 bash ~/miniconda.sh -b -p $HOME/miniconda
 
-echo 'debug.1'
-source $HOME/miniconda/bin/activate
-echo 'debug.2'
-
-export PATH="$HOME/miniconda/bin:$PATH"
-
+conda env create -n xDeepFM-criteo python=3.6
+ 
+# source $HOME/miniconda/bin/activate
+export PATH="$HOME/miniconda/envs/xDeepFM-criteo/bin:$PATH"
 echo $PATH
+
+# source $HOME/miniconda/bin/activate
+# export PATH="$HOME/miniconda/bin:$PATH"
+# echo $PATH
 cd examples/notebooks/xDeepFM
 
 echo 'debug.3'
-conda env create -f environment.yml
+conda env update -f environment.yml
 source activate xDeepFM-criteo
 
 bash ./run_notebooks.sh
