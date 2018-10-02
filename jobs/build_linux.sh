f49f99eb3427c7e3fdce893a8a34958f5e78b37b
Markus Cozowicz <marcozo@microsoft.com>
2018-10-02 12:30:43 +0100

    remove more linux build steps

--- jobs/build_linux.sh
+++ jobs/build_linux.sh
@@ -3,7 +3,7 @@ wget -q https://repo.continuum.io/miniconda/Miniconda3-3.7.0-Linux-x86_64.sh -O 
 bash ~/miniconda.sh -b -p $HOME/miniconda
 
 export PATH="$HOME/miniconda/bin:$PATH"
-conda create -n xDeepFM-criteo python=3.6
+conda create --verbose -n xDeepFM-criteo python=3.6
  
 # source $HOME/miniconda/bin/activate
 export PATH="$HOME/miniconda/envs/xDeepFM-criteo/bin:$PATH"
