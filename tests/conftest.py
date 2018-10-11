44c29061d770c0c5f4216a6b2a304452d271c91c
Max Kaznady <maxkaz@microsoft.com>
2018-10-11 18:47:38 +0000

    SAR: updated test marks and pySpark init

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,13 +26,11 @@ def start_spark_test(app_name="Sample", url="local[*]", memory="1G"):
         .config("spark.network.timeout", "10000000s")
         .config("spark.driver.maxResultSize", memory)
     """
-    spark = (
-        SparkSession.builder.appName(app_name)
-        .master(url)
-        .config("spark.driver.memory", memory)
-        .config("spark.sql.shuffle.partitions", "1")
+    spark = SparkSession.builder.appName(app_name) \
+        .master(url) \
+        .config("spark.driver.memory", memory) \
+        .config("spark.sql.shuffle.partitions", "1") \
         .getOrCreate()
-    )
 
     return spark
 
