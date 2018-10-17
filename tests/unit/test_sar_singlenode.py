a6b88603d3313af474c6e7f752459475e3b81494
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-10-17 14:43:36 +0100

    using pytest mark parametrize

--- tests/unit/test_sar_singlenode.py
+++ tests/unit/test_sar_singlenode.py
@@ -91,88 +91,6 @@ def get_train_test(load_pandas_dummy_timestamp_dataset):
 
 
 @pytest.fixture
-def load_dataset(load_pandas_dummy_timestamp_dataset):
-    """Load a fixture dataset"""
-    # TODO: DRY with get_train_test
-    trainset, testset = train_test_split(
-        load_pandas_dummy_timestamp_dataset,
-        test_size=None,
-        train_size=0.8,
-        random_state=0,
-    )
-    return trainset, testset
-
-
-def _sar_hash(train, test, header, pandas_new=False):
-    # TODO: review this function
-    # index all users and items which SAR will compute scores for
-    # bugfix to get around different pandas vesions in build servers
-    if test is not None:
-        if pandas_new:
-            df_all = pd.concat([train, test], sort=False)
-        else:
-            df_all = pd.concat([train, test])
-    else:
-        df_all = train
-
-    # hash SAR
-    # Obtain all the users and items from both training and test data
-    unique_users = df_all[header["col_user"]].unique()
-    unique_items = df_all[header["col_item"]].unique()
-
-    # Hash users and items to smaller continuous space.
-    # Actually, this is an ordered set - it's discrete, but contiguous.
-    # This helps keep the matrices we keep in memory as small as possible.
-    enumerate_items_1, enumerate_items_2 = itertools.tee(enumerate(unique_items))
-    enumerate_users_1, enumerate_users_2 = itertools.tee(enumerate(unique_users))
-    item_map_dict = {x: i for i, x in enumerate_items_1}
-    user_map_dict = {x: i for i, x in enumerate_users_1}
-
-    # the reverse of the dictionary above - array index to actual ID
-    index2user = dict(enumerate_users_2)
-    index2item = dict(enumerate_items_2)
-
-    return (
-        unique_users,
-        unique_items,
-        user_map_dict,
-        item_map_dict,
-        index2user,
-        index2item,
-    )
-
-
-def test_init(header):
-    model = SARSingleNodeReference(
-        remove_seen=True, similarity_type="jaccard", **header
-    )
-
-    assert model.col_user == "UserId"
-    assert model.col_item == "MovieId"
-    assert model.col_rating == "Rating"
-    # TODO: add more parameters
-
-
-def test_fit(get_train_test, header):
-    model = SARSingleNodeReference(
-        remove_seen=True, similarity_type="jaccard", **header
-    )
-    train, test = get_train_test
-    unique_users, unique_items, user_map_dict, item_map_dict, index2user, index2item = _sar_hash(
-        train, test, header
-    )
-    model.set_index(
-        unique_users, unique_items, user_map_dict, item_map_dict, index2user, index2item
-    )
-    model.fit(train)
-
-
-"""
-Fixtures to load and reconcile custom output from TLC
-"""
-
-
-@pytest.fixture
 def read_matrix(file, row_map=None, col_map=None):
     """read in test matrix and hash it"""
     reader = _csv_reader_url(file)
@@ -249,57 +167,98 @@ def load_demo_usage_data(header):
     return data
 
 
-"""
-Other fixtures are below
-"""
+def _sar_hash(train, test, header, pandas_new=False):
+    # TODO: review this function
+    # index all users and items which SAR will compute scores for
+    # bugfix to get around different pandas vesions in build servers
+    if test is not None:
+        if pandas_new:
+            df_all = pd.concat([train, test], sort=False)
+        else:
+            df_all = pd.concat([train, test])
+    else:
+        df_all = train
+
+    # hash SAR
+    # Obtain all the users and items from both training and test data
+    unique_users = df_all[header["col_user"]].unique()
+    unique_items = df_all[header["col_item"]].unique()
+
+    # Hash users and items to smaller continuous space.
+    # Actually, this is an ordered set - it's discrete, but contiguous.
+    # This helps keep the matrices we keep in memory as small as possible.
+    enumerate_items_1, enumerate_items_2 = itertools.tee(enumerate(unique_items))
+    enumerate_users_1, enumerate_users_2 = itertools.tee(enumerate(unique_users))
+    item_map_dict = {x: i for i, x in enumerate_items_1}
+    user_map_dict = {x: i for i, x in enumerate_users_1}
+
+    # the reverse of the dictionary above - array index to actual ID
+    index2user = dict(enumerate_users_2)
+    index2item = dict(enumerate_items_2)
+
+    return (
+        unique_users,
+        unique_items,
+        user_map_dict,
+        item_map_dict,
+        index2user,
+        index2item,
+    )
 
 
-def sar_algo(header):
-    """Add different SAR algos"""
-    return [
-        [
-            SARSingleNodeReference(
-                remove_seen=True, similarity_type="jaccard", **header
-            ),
-            "sar_ref",
-        ],
-        [
-            SARSingleNodeReference(
-                remove_seen=True,
-                similarity_type="jaccard",
-                time_decay_coefficient=30,
-                timedecay_formula=True,
-                **header
-            ),
-            "sar_ref",
-        ],
-    ]
+def test_init(header):
+    model = SARSingleNodeReference(
+        remove_seen=True, similarity_type="jaccard", **header
+    )
+
+    assert model.col_user == "UserId"
+    assert model.col_item == "MovieId"
+    assert model.col_rating == "Rating"
+    # TODO: add more parameters
 
 
-def test_predict(load_dataset, header):
-    """Test algo predict"""
-    trainset, testset = load_dataset
+@pytest.mark.parametrize("similarity_type, timedecay_formula", [
+    ("jaccard", False),
+    ("lift", True),
+])
+def test_fit(similarity_type, timedecay_formula, get_train_test, header):
+    model = SARSingleNodeReference(
+        remove_seen=True, similarity_type=similarity_type, timedecay_formula=timedecay_formula, **header
+    )
+    trainset, testset = get_train_test
+    unique_users, unique_items, user_map_dict, item_map_dict, index2user, index2item = _sar_hash(
+        trainset, testset, header
+    )
+    model.set_index(
+        unique_users, unique_items, user_map_dict, item_map_dict, index2user, index2item
+    )
+    model.fit(trainset)
+
+
+@pytest.mark.parametrize("similarity_type, timedecay_formula", [
+    ("jaccard", False),
+    ("lift", True),
+])
+def test_predict(similarity_type, timedecay_formula, get_train_test, header):
+    model = SARSingleNodeReference(
+        remove_seen=True, similarity_type=similarity_type, timedecay_formula=timedecay_formula, **header
+    )
+    trainset, testset = get_train_test
 
     unique_users, unique_items, user_map_dict, item_map_dict, index2user, index2item = _sar_hash(
         trainset, testset, header
     )
-
-    for recommender in sar_algo(header):
-        recommender[0].set_index(
-            unique_users,
-            unique_items,
-            user_map_dict,
-            item_map_dict,
-            index2user,
-            index2item,
-        )
-        recommender[0].fit(trainset)
-        preds = recommender[0].predict(testset)
-        assert len(preds) == 2
-        assert isinstance(preds, pd.DataFrame)
-        assert preds[header["col_user"]].dtype == object
-        assert preds[header["col_item"]].dtype == object
-        assert preds[PREDICTION_COL].dtype == float
+    model.set_index(
+        unique_users, unique_items, user_map_dict, item_map_dict, index2user, index2item
+    )
+    model.fit(trainset)
+    preds = model.predict(testset)
+    
+    assert len(preds) == 2
+    assert isinstance(preds, pd.DataFrame)
+    assert preds[header["col_user"]].dtype == object
+    assert preds[header["col_item"]].dtype == object
+    assert preds[PREDICTION_COL].dtype == float
 
 
 """
@@ -307,11 +266,8 @@ Main SAR tests are below - load test files which are used for both Scala SAR and
 """
 
 # Tests 1-6
-params = "threshold,similarity_type,file"
-
-
 @pytest.mark.parametrize(
-    params,
+    "threshold,similarity_type,file",
     [
         (1, "cooccurrence", "count"),
         (1, "jaccard", "jac"),
