505b2da07f166fb6f1c82b8055cc5064a010daad
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-10-22 12:43:07 +0200

    test data_split and sar deep dive fixes #103

--- tests/unit/test_notebook_pyspark.py
+++ tests/unit/test_notebook_pyspark.py
@@ -16,7 +16,11 @@ def notebooks():
     paths = {
         "sar_pyspark": os.path.join(
             folder_notebooks, "00_quick_start", "sar_pyspark_movielens.ipynb"
-        )
+        ),
+        "data_split": os.path.join(folder_notebooks, "01_data", "data_split.ipynb"),
+        "sar_deep_dive": os.path.join(
+            folder_notebooks, "02_modeling", "sar_deep_dive.ipynb"
+        ),
     }
     return paths
 
@@ -25,4 +29,18 @@ def notebooks():
 @pytest.mark.spark
 def test_sar_single_node_runs(notebooks):
     notebook_path = notebooks["sar_pyspark"]
+    pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK)
+
+
+@pytest.mark.notebooks
+@pytest.mark.spark
+def test_data_split_runs(notebooks):
+    notebook_path = notebooks["data_split"]
+    pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK)
+
+
+@pytest.mark.notebooks
+@pytest.mark.spark
+def test_sar_deep_dive_runs(notebooks):
+    notebook_path = notebooks["sar_deep_dive"]
     pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK)
