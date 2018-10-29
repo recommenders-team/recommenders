6a91ccbc829c8bbd07d0fabe492f791f32b50fad
Markus Cozowicz <eisber@eisber.net>
2018-10-29 19:26:23 +0100

    improved documentation; reverted unit test refactoring

--- tests/unit/test_notebooks_pyspark.py
+++ tests/unit/test_notebooks_pyspark.py
@@ -4,30 +4,69 @@ import pandas as pd
 import papermill as pm
 from tests.unit.notebooks_common import path_notebooks, OUTPUT_NOTEBOOK, KERNEL_NAME
 
-notebooks = [
-    (
-        "sar_pyspark",
-        os.path.join(path_notebooks(), "00_quick_start", "sar_pyspark_movielens.ipynb"),
-    ),
-    (
-        "sarplus_movielens",
-        os.path.join(path_notebooks(), "00_quick_start", "sarplus_movielens.ipynb"),
-    ),
-    ("data_split", os.path.join(path_notebooks(), "01_data", "data_split.ipynb")),
-    (
-        "sar_deep_dive",
-        os.path.join(path_notebooks(), "02_modeling", "sar_deep_dive.ipynb"),
-    ),
-    (
-        "als_deep_dive",
-        os.path.join(path_notebooks(), "02_modeling", "als_deep_dive.ipynb"),
-    ),
-    ("evaluation", os.path.join(path_notebooks(), "03_evaluate", "evaluation.ipynb")),
-]
+
+@pytest.fixture(scope="module")
+def notebooks():
+    folder_notebooks = path_notebooks()
+
+    # Path for the notebooks
+    paths = {
+        "sar_pyspark": os.path.join(
+            folder_notebooks, "00_quick_start", "sar_pyspark_movielens.ipynb"
+        ),
+        "sarplus_movielens": os.path.join(
+            folder_notebooks, "00_quick_start", "sarplus_movielens.ipynb"
+        ),
+        "data_split": os.path.join(folder_notebooks, "01_data", "data_split.ipynb"),
+        "sar_deep_dive": os.path.join(
+            folder_notebooks, "02_modeling", "sar_deep_dive.ipynb"
+        ),
+        "als_deep_dive": os.path.join(
+            folder_notebooks, "02_modeling", "als_deep_dive.ipynb"
+        ),
+        "evaluation": os.path.join(folder_notebooks, "03_evaluate", "evaluation.ipynb"),
+    }
+    return paths
 
 
 @pytest.mark.notebooks
 @pytest.mark.spark
-@pytest.mark.parametrize("name,notebook_path", notebooks)
-def test_sar_single_node_runs(name, notebook_path):
+def test_sar_single_node_runs(notebooks):
+    notebook_path = notebooks["sar_pyspark"]
     pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
+
+
+@pytest.mark.notebooks
+@pytest.mark.spark
+def test_sarplus(notebooks):
+    notebook_path = notebooks["sarplus_movielens"]
+    pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
+
+
+@pytest.mark.notebooks
+@pytest.mark.spark
+def test_data_split_runs(notebooks):
+    notebook_path = notebooks["data_split"]
+    pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
+
+
+@pytest.mark.notebooks
+@pytest.mark.spark
+def test_sar_deep_dive_runs(notebooks):
+    notebook_path = notebooks["sar_deep_dive"]
+    pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
+
+
+@pytest.mark.notebooks
+@pytest.mark.spark
+def test_als_deep_dive_runs(notebooks):
+    notebook_path = notebooks["als_deep_dive"]
+    pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
+
+
+@pytest.mark.notebooks
+@pytest.mark.spark
+def test_evaluation_runs(notebooks):
+    notebook_path = notebooks["evaluation"]
+    pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
+
