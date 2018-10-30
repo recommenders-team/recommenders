950a876d43335601d6ba8a3d2676b997283ac663
Le Zhang <zhle@microsoft.com>
2018-10-30 08:56:45 +0000

    SPLITTER: python stratified splitter

--- reco_utils/dataset/python_splitters.py
+++ reco_utils/dataset/python_splitters.py
@@ -102,3 +102,64 @@ def python_chrono_split(
             splits[x] = pd.concat([splits[x], group_splits[x]])
 
     return splits
+
+
+def python_stratified_split(
+        data,
+        ratio=0.75,
+        min_rating=1,
+        filter_by="user",
+        col_user=DEFAULT_USER_COL,
+        col_item=DEFAULT_ITEM_COL
+):
+    """Pandas stratified splitter
+
+    Args:
+        data (pd.DataFrame): Pandas DataFrame to be split.
+        ratio (float or list): Ratio for splitting data. If it is a single float number
+            it splits data into two halfs and the ratio argument indicates the ratio of
+            training data set; if it is a list of float numbers, the splitter splits
+            data into several portions corresponding to the split ratios. If a list is
+            provided and the ratios are not summed to 1, they will be normalized.
+        seed (int): Seed.
+        min_rating (int): minimum number of ratings for user or item.
+        filter_by (str): either "user" or "item", depending on which of the two is to
+            filter with min_rating.
+        col_user (str): column name of user IDs.
+        col_item (str): column name of item IDs.
+
+    Returns:
+        list: Splits of the input data as pd.DataFrame.
+    """
+    if not (filter_by == "user" or filter_by == "item"):
+        raise ValueError("filter_by should be either 'user' or 'item'.")
+
+    if min_rating < 1:
+        raise ValueError("min_rating should be integer and larger than or equal to 1.")
+
+    multi_split, ratio = process_split_ratio(ratio)
+
+    split_by_column = col_user if filter_by == "user" else col_item
+
+    ratio = ratio if multi_split else [ratio, 1 - ratio]
+
+    if min_rating > 1:
+        data = min_rating_filter(
+            data,
+            min_rating=min_rating,
+            filter_by=filter_by,
+            col_user=col_user,
+            col_item=col_item,
+        )
+
+    num_of_splits = len(ratio)
+    splits = [pd.DataFrame({})] * num_of_splits
+    df_grouped = data.groupby(split_by_column)
+    for name, group in df_grouped:
+        group_splits = split_pandas_data_with_ratios(
+            df_grouped.get_group(name), ratio, resample=True
+        )
+        for x in range(num_of_splits):
+            splits[x] = pd.concat([splits[x], group_splits[x]])
+
+    return splits
