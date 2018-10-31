592318d758ab318b69fa5eed9edf1c0c7e23cd17
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-10-31 12:12:56 +0000

    smoke test spark

---
+++ tests/smoke/test_notebooks_pyspark.py
@@ -0,0 +1,30 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+import pytest
+import papermill as pm
+from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
+
+
+@pytest.mark.smoke
+@pytest.mark.spark
+def test_als_pyspark_smoke(notebooks):
+    notebook_path = notebooks["als_pyspark"]
+    pm.execute_notebook(
+        notebook_path,
+        OUTPUT_NOTEBOOK,
+        kernel_name=KERNEL_NAME,
+        parameters=dict(TOP_K=10, MOVIELENS_DATA_SIZE="100k"),
+    )
+    nb = pm.read_notebook(OUTPUT_NOTEBOOK)
+    df = nb.dataframe
+    result_map = df.loc[df["name"] == "map", "value"].values[0]
+    assert result_map > 0.02
+    result_ndcg = df.loc[df["name"] == "ndcg", "value"].values[0]
+    assert result_ndcg > 0.10
+    result_precision = df.loc[df["name"] == "precision", "value"].values[0]
+    assert result_precision > 0.10
+    result_recall = df.loc[df["name"] == "recall", "value"].values[0]
+    assert result_recall > 0.07
+
