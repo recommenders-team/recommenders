5cd7a29b14cfaf3dce50a580c64478bdeb3d2e03
Markus Cozowicz <eisber@eisber.net>
2018-11-01 00:45:57 +0100

    added pybind11 and fixed pysarplus dependency

--- scripts/generate_conda_file.sh
+++ scripts/generate_conda_file.sh
@@ -96,7 +96,8 @@ ${pyspark}- pyspark==2.3.1
   - pytest==3.6.4
   - papermill>=0.15.0
   - black>=18.6b4
-  - pysarplus>=0.2.3
+  - pysarplus>=0.2.2
+  - pybind11>=2.2
 EOM
 
 echo "Conda file generated: " $CONDA_FILE
