93e32374f31630f4983a60b2d677ad7d1153885f
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-10-31 13:31:31 +0000

    integration tests python

---
+++ tests/integration/test_notebooks_python.py
@@ -0,0 +1,38 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+import pytest
+import papermill as pm
+from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    "size, result_list",
+    [
+        ("1m", [0.06, 0.30, 0.27, 0.10]),
+        ("10m", [0.10, 0.32, 0.27, 0.15]),
+        #       ("20m", [0.03, 0.15, 0.14, 0.08]), # raises MemoryError
+    ],
+)
+def test_als_pyspark_integration(notebooks, size, result_list):
+    notebook_path = notebooks["sar_single_node"]
+    pm.execute_notebook(
+        notebook_path,
+        OUTPUT_NOTEBOOK,
+        kernel_name=KERNEL_NAME,
+        parameters=dict(TOP_K=10, MOVIELENS_DATA_SIZE=size),
+    )
+    nb = pm.read_notebook(OUTPUT_NOTEBOOK)
+    df = nb.dataframe
+    result_map = df.loc[df["name"] == "map", "value"].values[0]
+    assert result_map > result_list[0]
+    result_ndcg = df.loc[df["name"] == "ndcg", "value"].values[0]
+    assert result_ndcg > result_list[1]
+    result_precision = df.loc[df["name"] == "precision", "value"].values[0]
+    assert result_precision > result_list[2]
+    result_recall = df.loc[df["name"] == "recall", "value"].values[0]
+    assert result_recall > result_list[3]
+    print(result_map, result_ndcg, result_precision, result_recall)
+
