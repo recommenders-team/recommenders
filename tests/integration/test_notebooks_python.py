879415a81233b9ac962ef54bf75420fb97b6382d
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-11-01 10:15:21 +0000

    change assert to approx equal

--- tests/integration/test_notebooks_python.py
+++ tests/integration/test_notebooks_python.py
@@ -7,6 +7,9 @@ import papermill as pm
 from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
 
 
+TOL = 0.5
+
+
 @pytest.mark.integration
 @pytest.mark.parametrize(
     "size, result_list",
@@ -27,12 +30,11 @@ def test_als_pyspark_integration(notebooks, size, result_list):
     nb = pm.read_notebook(OUTPUT_NOTEBOOK)
     df = nb.dataframe
     result_map = df.loc[df["name"] == "map", "value"].values[0]
-    assert result_map > result_list[0]
+    assert result_map == pytest.approx(result_list[0], TOL)
     result_ndcg = df.loc[df["name"] == "ndcg", "value"].values[0]
-    assert result_ndcg > result_list[1]
+    assert result_ndcg == pytest.approx(result_list[1], TOL)
     result_precision = df.loc[df["name"] == "precision", "value"].values[0]
-    assert result_precision > result_list[2]
+    assert result_precision == pytest.approx(result_list[2], TOL)
     result_recall = df.loc[df["name"] == "recall", "value"].values[0]
-    assert result_recall > result_list[3]
-    print(result_map, result_ndcg, result_precision, result_recall)
+    assert result_recall == pytest.approx(result_list[3], TOL)
 
--- tests/smoke/test_notebooks_python.py
+++ tests/smoke/test_notebooks_python.py
@@ -8,6 +8,9 @@ import papermill as pm
 from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
 
 
+TOL = 0.5
+
+
 @pytest.mark.smoke
 def test_sar_single_node_smoke(notebooks):
     notebook_path = notebooks["sar_single_node"]
@@ -21,10 +24,10 @@ def test_sar_single_node_smoke(notebooks):
     nb = pm.read_notebook(OUTPUT_NOTEBOOK)
     df = nb.dataframe
     result_map = df.loc[df["name"] == "map", "value"].values[0]
-    assert result_map > 0.10
+    assert result_map == pytest.approx(0.10, TOL)
     result_ndcg = df.loc[df["name"] == "ndcg", "value"].values[0]
-    assert result_ndcg > 0.37
+    assert result_ndcg == pytest.approx(0.37, TOL)
     result_precision = df.loc[df["name"] == "precision", "value"].values[0]
-    assert result_precision > 0.32
+    assert result_precision == pytest.approx(0.32, TOL)
     result_recall = df.loc[df["name"] == "recall", "value"].values[0]
-    assert result_recall > 0.17
+    assert result_recall == pytest.approx(0.17, TOL)
