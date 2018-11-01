c219349e1f0eaabb152590b98f9ba7501a17f794
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-11-01 10:15:21 +0000

    change assert to approx equal

--- tests/smoke/test_notebooks_pyspark.py
+++ tests/smoke/test_notebooks_pyspark.py
@@ -7,6 +7,9 @@ import papermill as pm
 from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
 
 
+TOL = 0.5
+
+
 @pytest.mark.smoke
 @pytest.mark.spark
 def test_als_pyspark_smoke(notebooks):
@@ -20,11 +23,11 @@ def test_als_pyspark_smoke(notebooks):
     nb = pm.read_notebook(OUTPUT_NOTEBOOK)
     df = nb.dataframe
     result_map = df.loc[df["name"] == "map", "value"].values[0]
-    assert result_map > 0.02
+    assert result_map == pytest.approx(0.02, TOL)
     result_ndcg = df.loc[df["name"] == "ndcg", "value"].values[0]
-    assert result_ndcg > 0.10
+    assert result_ndcg == pytest.approx(0.10, TOL
     result_precision = df.loc[df["name"] == "precision", "value"].values[0]
-    assert result_precision > 0.10
+    assert result_precision == pytest.approx(0.10, TOL)
     result_recall = df.loc[df["name"] == "recall", "value"].values[0]
-    assert result_recall > 0.07
+    assert result_recall == pytest.approx(0.07, TOL)
 
