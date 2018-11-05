ef215a7d1aad97a3f4e629d3a5be0ab03ec10a32
Markus Cozowicz <eisber@eisber.net>
2018-11-05 16:03:04 +0100

    initial sarplus implementation

---
+++ sarplus/scala/python/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pysarplus_dummy',
+      version='0.2',
+      description='pysarplus dummy package to trigger spark packaging',
+      author='Markus Cozowicz',
+      author_email='m@cozowicz.at',
+      url='https://github.com/eisber/sarplus',
+      packages=['pysarplus_dummy'],
+     )
