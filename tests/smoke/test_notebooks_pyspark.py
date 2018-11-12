36c6d018f4b1723e7db04fae590b2da7c72cb5a1
Jun Ki Min <loomlike@gmail.com>
2018-11-12 17:12:19 +0000

    ALS notebook smoke and integration test
    Add rating metrics evaluation to smoke and integration test

--- tests/smoke/test_notebooks_pyspark.py
+++ tests/smoke/test_notebooks_pyspark.py
@@ -31,3 +31,12 @@ def test_als_pyspark_smoke(notebooks):
     result_recall = df.loc[df["name"] == "recall", "value"].values[0]
     assert result_recall == pytest.approx(0.07, TOL)
 
+    result_rmse = df.loc[df["name"] == "rmse", "value"].values[0]
+    assert result_rmse == pytest.approx(1.12, TOL)
+    result_mae = df.loc[df["name"] == "mae", "value"].values[0]
+    assert result_mae == pytest.approx(0.87, TOL)
+    result_exp_var = df.loc[df["name"] == "exp_var", "value"].values[0]
+    assert result_exp_var == pytest.approx(0.01, TOL)
+    result_rsquared = df.loc[df["name"] == "rsquared", "value"].values[0]
+    assert result_rsquared == pytest.approx(0.00, TOL)
+
