7af4b57785e6a35166240b932b1b4e33ff7b1da1
Jun Ki Min <42475935+loomlike@users.noreply.github.com>
2018-11-15 18:29:14 -0500

    Notebook utils unit test - resolve comment
    Remove pytest mark notebook as this is testing python script module
    Remove is_databricks test since we cannot test true-positive case
    Change test_notebook_utils.ipynb to record results on papermill

--- tests/unit/test_notebook_utils.py
+++ tests/unit/test_notebook_utils.py
@@ -4,32 +4,26 @@
 import pytest
 import papermill as pm
 from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
-from reco_utils.common.notebook_utils import is_jupyter, is_databricks
+from reco_utils.common.notebook_utils import is_jupyter
 
 
-@pytest.mark.notebooks
 def test_is_jupyter():
     """Only can test if the module is running on non-Databricks
     """
+    # Test on the terminal
     assert is_jupyter() is False
-    # Test the function on Jupyter notebook
+
+    # Test on Jupyter notebook
     pm.execute_notebook(
         'test_notebook_utils.ipynb',
         OUTPUT_NOTEBOOK,
-        parameters=dict(FUNCTION_NAME='is_jupyter'),
         kernel_name=KERNEL_NAME,
     )
+    nb = pm.read_notebook(OUTPUT_NOTEBOOK)
+    df = nb.dataframe
+    result_is_jupyter = df.loc[df["name"] == "is_jupyter", "value"].values[0]
+    assert result_is_jupyter is True
 
-
-@pytest.mark.notebooks
-def test_is_databricks():
-    """Only can test if the module is running on non-Databricks
-    """
-    assert is_databricks() is False
-    # Test the function on Jupyter notebook
-    pm.execute_notebook(
-        'test_notebook_utils.ipynb',
-        OUTPUT_NOTEBOOK,
-        parameters=dict(FUNCTION_NAME='is_databricks'),
-        kernel_name=KERNEL_NAME,
-    )
+# @pytest.mark.notebooks
+# def test_is_databricks():
+#     TODO Currently, we cannot pytest modules on Databricks
