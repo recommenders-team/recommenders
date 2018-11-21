264fe9d44bdf2f41e35d37d9fc62c1c80ed2f420
WessZumino <milletari@gmail.com>
2018-11-21 15:50:31 +0800

    momentum optimizer fully integrated

--- reco_utils/recommender/rbm/__init__.py
+++ reco_utils/recommender/rbm/__init__.py
@@ -5,11 +5,15 @@ HIDDEN = 10
 
 #keep probability for deopout regularization
 KEEP_PROB = 1
+
+#Initial value of the momentum optimizer
+MOMENTUM = 0.9
+
 #standard deviation for initializing the weights from a normal distribution with zero mean
 STDV =  0.01
 #Learning rate
 ALPHA = 0.004
 #number of minibatches
 MINIBATCH = 100
-#number of epochs for the optimization 
+#number of epochs for the optimization
 EPOCHS = 20
