067ef48d3a6cf61d910955dac2e2a792e5ae92f7
WessZumino <milletari@gmail.com>
2018-11-28 13:29:21 +0800

    debug file updated
    Former-commit-id: 69247029651eef71de73e0fbcd7cc6e251129a50

--- reco_utils/recommender/rbm/rbm_movie.py
+++ reco_utils/recommender/rbm/rbm_movie.py
@@ -105,11 +105,12 @@ header = {
 
 data = splitter(DF = ratings_df, **header)
 
-Xtr, Xtst = data.stratified_split()
+Xtr, Xtst, train_df, test_df, map = data.stratified_split()
 
-#generate the ranking matrix. Movies that haven not been rated yet get a 0 rating.
-Xtr.shape
-Xtst.shape
+
+train_df.head()
+
+test_df.head()
 
 #Check the number of elements
 (Xtr !=0).sum()
@@ -141,17 +142,21 @@ zero_test/total *100
 
 model = RBM(hidden_units= 500, save_model=True, keep_prob= .7, training_epoch = 10, **header)
 
-model.fit(Xtr)
+model.fit(Xtr,Xtst)
 
 
 
 #predict
-top_k_df =  model.recommend_k_items(Xtr)
+top_k = 10
+results, top_items =  model.recommend_k_items(Xtr, map)
 
+results.head(10)
+
+#Check if the mapping is correct
+[ map[1][top_items[0][i]] for i in range(10) ]
 
 
 #----------------------------------Create a user report with recommendations-----------------------------------------
-usr_id = 0
 
 usr_mv_like = np.where(vp[usr_id]==2)
 mv_id = (np.asanyarray(usr_mv_like)+1).flatten()
