373e50f03b41af780b54e2654ebbcb9735b5429a
yueguoguo <yueguoguo1024@gmail.com>
2018-12-01 09:37:06 +0000

    TEST: update smoke test target values

--- tests/smoke/test_notebooks_pyspark.py
+++ tests/smoke/test_notebooks_pyspark.py
@@ -23,20 +23,20 @@ def test_als_pyspark_smoke(notebooks):
     nb = pm.read_notebook(OUTPUT_NOTEBOOK)
     df = nb.dataframe
     result_map = df.loc[df["name"] == "map", "value"].values[0]
-    assert result_map == pytest.approx(0.003317, TOL)
+    assert result_map == pytest.approx(0.004745, TOL)
     result_ndcg = df.loc[df["name"] == "ndcg", "value"].values[0]
-    assert result_ndcg == pytest.approx(0.032687, TOL)
+    assert result_ndcg == pytest.approx(0.044285, TOL)
     result_precision = df.loc[df["name"] == "precision", "value"].values[0]
-    assert result_precision == pytest.approx(0.038070, TOL)
+    assert result_precision == pytest.approx(0.047240, TOL)
     result_recall = df.loc[df["name"] == "recall", "value"].values[0]
-    assert result_recall == pytest.approx(0.014183, TOL)
+    assert result_recall == pytest.approx(0.016443, TOL)
 
     result_rmse = df.loc[df["name"] == "rmse", "value"].values[0]
-    assert result_rmse == pytest.approx(0.972540, TOL)
+    assert result_rmse == pytest.approx(0.95, TOL)
     result_mae = df.loc[df["name"] == "mae", "value"].values[0]
-    assert result_mae == pytest.approx(0.754861, TOL)
+    assert result_mae == pytest.approx(0.744727, TOL)
     result_exp_var = df.loc[df["name"] == "exp_var", "value"].values[0]
-    assert result_exp_var == pytest.approx(0.263450, TOL)
+    assert result_exp_var == pytest.approx(0.287247, TOL)
     result_rsquared = df.loc[df["name"] == "rsquared", "value"].values[0]
-    assert result_rsquared == pytest.approx(0.267639, TOL)
+    assert result_rsquared == pytest.approx(0.281822, TOL)
 
