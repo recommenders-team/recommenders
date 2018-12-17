e9645ae84674519e0343efddeef661e468b0a48b
Scott Graham <>
2018-12-17 12:26:19 -0500

    setting a random seed value for ALS in als pyspark notebook, updating tests to stop spark session to help with disk cleanup, and dropping 10m and 20m integration tests

--- tests/smoke/test_notebooks_pyspark.py
+++ tests/smoke/test_notebooks_pyspark.py
@@ -1,13 +1,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import os
 import pytest
 import papermill as pm
 from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
+from reco_utils.common.spark_utils import start_or_get_spark
 
 
-TOL = 0.1
+TOL = 0.05
 
 
 @pytest.mark.smoke
@@ -21,22 +21,14 @@ def test_als_pyspark_smoke(notebooks):
         parameters=dict(TOP_K=10, MOVIELENS_DATA_SIZE="100k"),
     )
     nb = pm.read_notebook(OUTPUT_NOTEBOOK)
-    df = nb.dataframe
-    result_map = df.loc[df["name"] == "map", "value"].values[0]
-    assert result_map == pytest.approx(0.004, TOL)
-    result_ndcg = df.loc[df["name"] == "ndcg", "value"].values[0]
-    assert result_ndcg == pytest.approx(0.044285, TOL)
-    result_precision = df.loc[df["name"] == "precision", "value"].values[0]
-    assert result_precision == pytest.approx(0.047240, TOL)
-    result_recall = df.loc[df["name"] == "recall", "value"].values[0]
-    assert result_recall == pytest.approx(0.016443, TOL)
+    results = nb.dataframe.set_index("name")["value"]
+    start_or_get_spark("ALS PySpark").stop()
 
-    result_rmse = df.loc[df["name"] == "rmse", "value"].values[0]
-    assert result_rmse == pytest.approx(0.95, TOL)
-    result_mae = df.loc[df["name"] == "mae", "value"].values[0]
-    assert result_mae == pytest.approx(0.744727, TOL)
-    result_exp_var = df.loc[df["name"] == "exp_var", "value"].values[0]
-    assert result_exp_var == pytest.approx(0.287247, TOL)
-    result_rsquared = df.loc[df["name"] == "rsquared", "value"].values[0]
-    assert result_rsquared == pytest.approx(0.281822, TOL)
-
+    assert results["map"] == pytest.approx(0.00481, rel=TOL)
+    assert results["ndcg"] == pytest.approx(0.04289, rel=TOL)
+    assert results["precision"] == pytest.approx(0.047558, rel=TOL)
+    assert results["recall"] == pytest.approx(0.018512, rel=TOL)
+    assert results["rmse"] == pytest.approx(0.950697, rel=TOL)
+    assert results["mae"] == pytest.approx(0.7424, rel=TOL)
+    assert results["exp_var"] == pytest.approx(0.285606, rel=TOL)
+    assert results["rsquared"] == pytest.approx(0.280812, rel=TOL)
