91b5bf98f32f664186454a9caf8717594a2f6ee5
Scott Graham <>
2018-12-18 17:17:34 -0500

    updating notebooks and tests to capture correct values / tolerances, also small cleanup to redundant .gitignore items

--- tests/unit/test_notebooks_pyspark.py
+++ tests/unit/test_notebooks_pyspark.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import os
 import pytest
 import papermill as pm
 from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
