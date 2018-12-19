bb0bde7faf06821911630741a189e07bdba7c5fc
Jun Ki Min <42475935+loomlike@users.noreply.github.com>
2018-12-19 15:39:38 -0500

    Add tests

--- tests/smoke/test_notebooks_python.py
+++ tests/smoke/test_notebooks_python.py
@@ -25,3 +25,25 @@ def test_sar_single_node_smoke(notebooks):
     assert results["ndcg"] == pytest.approx(0.373197255, TOL)
     assert results["precision"] == pytest.approx(0.326617179, TOL)
     assert results["recall"] == pytest.approx(0.175956743, TOL)
+
+
+@pytest.mark.smoke
+def test_baseline_estimation_smoke(notebooks):
+    notebook_path = notebooks["baseline_estimation"]
+    pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
+    pm.execute_notebook(
+        notebook_path,
+        OUTPUT_NOTEBOOK,
+        kernel_name=KERNEL_NAME,
+        parameters=dict(TOP_K=10, MOVIELENS_DATA_SIZE="100k"),
+    )
+    results = pm.read_notebook(OUTPUT_NOTEBOOK).dataframe.set_index("name")["value"]
+
+    assert results["rmse"] == pytest.approx(1.054252, TOL)
+    assert results["mae"] == pytest.approx(0.846033, TOL)
+    assert results["rsquared"] == pytest.approx(0.136435, TOL)
+    assert results["exp_var"] == pytest.approx(0.136446, TOL)
+    assert results["map"] == pytest.approx(0.052850, TOL)
+    assert results["ndcg"] == pytest.approx(0.248061, TOL)
+    assert results["precision"] == pytest.approx(0.223754, TOL)
+    assert results["recall"] == pytest.approx(0.108826, TOL)
