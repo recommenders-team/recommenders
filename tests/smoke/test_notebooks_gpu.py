27a1d04e4e731a81ef70b405b6191708447c67c8
yueguoguo <yueguoguo1024@gmail.com>
2019-01-21 09:17:39 +0000

    TESTS: add ncf smoke test

--- tests/smoke/test_notebooks_gpu.py
+++ tests/smoke/test_notebooks_gpu.py
@@ -1,8 +1,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+import papermill as pm
 import pytest
 from reco_utils.common.gpu_utils import get_number_gpus
+from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
+
+
+TOL = 0.05
 
 
 @pytest.mark.smoke
@@ -10,3 +15,21 @@ from reco_utils.common.gpu_utils import get_number_gpus
 def test_gpu_vm():
     assert get_number_gpus() >= 1
 
+
+@pytest.mark.smoke
+@pytest.mark.gpu
+def test_ncf_smoke(notebooks):
+    notebook_path = notebooks["ncf"]
+    pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
+    pm.execute_notebook(
+        notebook_path,
+        OUTPUT_NOTEBOOK,
+        kernel_name=KERNEL_NAME,
+        parameters=dict(TOP_K=10, MOVIELENS_DATA_SIZE="100k"),
+    )
+    results = pm.read_notebook(OUTPUT_NOTEBOOK).dataframe.set_index("name")["value"]
+
+    assert results["map"] == pytest.approx(0.048396, TOL)
+    assert results["ndcg"] == pytest.approx(0.193960, TOL)
+    assert results["precision"] == pytest.approx(0.172428, TOL)
+    assert results["recall"] == pytest.approx(0.097780, TOL)
