8083e1de0b79fcf6a6c994d97e09cd53776c4ea6
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-01-26 09:56:08 +0000

    unit tests ncf

--- tests/unit/test_notebooks_gpu.py
+++ tests/unit/test_notebooks_gpu.py
@@ -15,8 +15,37 @@ def test_gpu_vm():
 @pytest.mark.notebooks
 @pytest.mark.gpu
 def test_fastai(notebooks):
-    assert get_number_gpus() >= 1
-
     notebook_path = notebooks["fastai"]
-    print('running fastai unit test')
     pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
+
+
+@pytest.mark.notebooks
+@pytest.mark.gpu
+def test_ncf(notebooks):
+    notebook_path = notebooks["ncf"]
+    pm.execute_notebook(notebook_path, 
+                        OUTPUT_NOTEBOOK, 
+                        kernel_name=KERNEL_NAME,
+                        parameters=dict(TOP_K=10, 
+                                        MOVIELENS_DATA_SIZE="100k",
+                                        EPOCHS=1,
+                                        BATCH_SIZE=1024),
+                       )
+
+
+@pytest.mark.notebooks
+@pytest.mark.gpu
+def test_ncf_deep_dive(notebooks):
+    notebook_path = notebooks["ncf_deep_dive"]
+    pm.execute_notebook(notebook_path, 
+                        OUTPUT_NOTEBOOK, 
+                        kernel_name=KERNEL_NAME,
+                        parameters=dict(TOP_K=10, 
+                                        MOVIELENS_DATA_SIZE="100k",
+                                        EPOCHS=1,
+                                        BATCH_SIZE=1024),
+                       )
+
+
+
+
