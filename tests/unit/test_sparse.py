6c60707df6c402ff6557f8435f130f7f2cab6ed8
WessZumino <milletari@gmail.com>
2019-01-28 14:33:38 +0800

    updated function names + added new test on sparse to df mapping

--- tests/unit/test_sparse.py
+++ tests/unit/test_sparse.py
@@ -6,9 +6,9 @@ import numpy as np
 import pytest
 from sklearn.utils import shuffle
 
-from reco_utils.dataset.sparse import affinity_matrix
+from reco_utils.dataset.sparse import AffinityMatrix
 
-from reco_utils.common.constants import(
+from reco_utils.common.constants import (
     DEFAULT_USER_COL,
     DEFAULT_ITEM_COL,
     DEFAULT_RATING_COL,
@@ -19,14 +19,10 @@ from reco_utils.common.constants import(
 @pytest.fixture(scope="module")
 def test_specs():
 
-    return {
-        "number_of_items": 50,
-        "number_of_users": 20,
-        "seed" :123
-        }
+    return {"number_of_items": 50, "number_of_users": 20, "seed": 123}
 
 
-#generate a syntetic dataset
+# generate a syntetic dataset
 @pytest.fixture(scope="module")
 def python_dataset(test_specs):
 
@@ -50,93 +46,106 @@ def python_dataset(test_specs):
 
         return random_dates
 
-    #fix the the random seed
+    # fix the the random seed
     np.random.seed(test_specs["seed"])
 
-    #generates the user/item affinity matrix. Ratings are from 1 to 5, with 0s denoting unrated items
-    X = np.random.randint(low= 0, high=6, size= ( test_specs["number_of_users"], test_specs["number_of_items"]) )
+    # generates the user/item affinity matrix. Ratings are from 1 to 5, with 0s denoting unrated items
+    X = np.random.randint(
+        low=0,
+        high=6,
+        size=(test_specs["number_of_users"], test_specs["number_of_items"]),
+    )
 
-    #In the main code, input data are passed as pandas dataframe. Below we generate such df from the above matrix
+    # In the main code, input data are passed as pandas dataframe. Below we generate such df from the above matrix
     userids = []
 
-    for i in range(1, test_specs["number_of_users"]+1):
-        userids.extend([i]*test_specs["number_of_items"])
+    for i in range(1, test_specs["number_of_users"] + 1):
+        userids.extend([i] * test_specs["number_of_items"])
 
-
-    itemids = [i for i in range(1, test_specs["number_of_items"]+1)]*test_specs["number_of_users"]
+    itemids = [i for i in range(1, test_specs["number_of_items"] + 1)] * test_specs[
+        "number_of_users"
+    ]
     ratings = np.reshape(X, -1)
 
-    #create dataframe
+    # create dataframe
     results = pd.DataFrame.from_dict(
-                        {
-                            DEFAULT_USER_COL: userids,
-                            DEFAULT_ITEM_COL: itemids,
-                            DEFAULT_RATING_COL: ratings,
-                            DEFAULT_TIMESTAMP_COL: random_date_generator(
-                                "2018-01-01", test_specs["number_of_users"]*test_specs["number_of_items"] ),
-                         }
-                    )
+        {
+            DEFAULT_USER_COL: userids,
+            DEFAULT_ITEM_COL: itemids,
+            DEFAULT_RATING_COL: ratings,
+            DEFAULT_TIMESTAMP_COL: random_date_generator(
+                "2018-01-01",
+                test_specs["number_of_users"] * test_specs["number_of_items"],
+            ),
+        }
+    )
 
-    #here we eliminate the missing ratings to obtain a standard form of the df as that of real data.
-    results = results[results.rating !=0]
+    # here we eliminate the missing ratings to obtain a standard form of the df as that of real data.
+    results = results[results.rating != 0]
 
-    #to make the df more realistic, we shuffle the rows of the df
-    rating = shuffle(results)
-
-    return rating
+    return results
 
 
-def test_df_to_sparse(test_specs, python_dataset ):
+def test_df_to_sparse(test_specs, python_dataset):
 
-    #generate a syntetic dataset
+    # generate a syntetic dataset
     df_rating = python_dataset
 
-    #initialize the splitter
+    # initialize the splitter
     header = {
-            "col_user": DEFAULT_USER_COL,
-            "col_item": DEFAULT_ITEM_COL,
-            "col_rating": DEFAULT_RATING_COL,
-        }
+        "col_user": DEFAULT_USER_COL,
+        "col_item": DEFAULT_ITEM_COL,
+        "col_rating": DEFAULT_RATING_COL,
+    }
 
-    #instantiate the splitter
-    am = affinity_matrix(DF = df_rating, **header)
+    # instantiate the affinity matrix
+    am = AffinityMatrix(DF=df_rating, **header)
 
-    #the splitter returns (in order): train and test user/affinity matrices, train and test datafarmes and user/items to matrix maps
+    # obtain the sparse matrix representation of the input dataframe
     X = am.gen_affinity_matrix()
 
-    #Tests
-    #check that the generated matrix has the correct dimensions
-    assert( (X.shape[0] == df_rating.userID.unique().shape[0]) & (X.shape[1] == df_rating.itemID.unique().shape[0]) )
+    # Tests
+    # check that the generated matrix has the correct dimensions
+    assert (X.shape[0] == df_rating.userID.unique().shape[0]) & (
+        X.shape[1] == df_rating.itemID.unique().shape[0]
+    )
 
 
-#def sparse_to_df(test_specs, python_dataset):
+# Test inverse mapping: from sparse matrix to dataframe
 
-    df_rating = python_dataset(test_specs())
 
-    #initialize the splitter
+def test_sparse_to_df(test_specs, python_dataset):
+
+    df_rating = python_dataset
+
+    # initialize the splitter
     header = {
-            "col_user": DEFAULT_USER_COL,
-            "col_item": DEFAULT_ITEM_COL,
-            "col_rating": DEFAULT_RATING_COL,
-        }
+        "col_user": DEFAULT_USER_COL,
+        "col_item": DEFAULT_ITEM_COL,
+        "col_rating": DEFAULT_RATING_COL,
+    }
 
-    #instantiate the splitter
-    am = affinity_matrix(DF = df_rating, **header)
+    # instantiate the the affinity matrix
+    am = AffinityMatrix(DF=df_rating, **header)
 
-    #the splitter returns (in order): train and test user/affinity matrices, train and test datafarmes and
-    #user/items to matrix maps
+    # generate the sparse matrix representation
     X = am.gen_affinity_matrix()
 
-    #use the inverse function to generate a pandas df from a sparse matrix ordered by userID
-    DF = am.map_back_sparse(X)
+    # use the inverse function to generate a pandas df from a sparse matrix ordered by userID
+    DF = am.map_back_sparse(X, kind="ratings")
 
-    #tests: check that the two dataframes have the same elements in the same positions.
+    # tests: check that the two dataframes have the same elements in the same positions.
+    assert (
+        DF.userID.values.all()
+        == df_rating.sort_values(by=["userID"]).userID.values.all()
+    )
 
-    assert DF.userID.values.all() == df_rating.sort_values(by=['userID']).userID.values.all()
+    assert (
+        DF.itemID.values.all()
+        == df_rating.sort_values(by=["userID"]).itemID.values.all()
+    )
 
-    assert DF.itemID.values.all() == df_rating.sort_values(by=['userID']).itemID.values.all()
-
-    assert DF.rating.values.all() == df_rating.sort_values(by=['userID']).rating.values.all()
-
-
-    
+    assert (
+        DF.rating.values.all()
+        == df_rating.sort_values(by=["userID"]).rating.values.all()
+    )
