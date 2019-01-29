1dd9c746f6a79c659448ff5689af47cc5cf54ef4
Jeremy Reynolds <jeremy.reynolds@microsoft.com>
2019-01-29 15:10:58 -0700

    Revert "update conda script to take a parameter for spark version"
    This reverts commit fb2f78b6deda891a70661a77830611d964e08d86.

--- scripts/generate_conda_file.sh
+++ scripts/generate_conda_file.sh
@@ -9,8 +9,6 @@
 # $ sh generate_conda_file.sh --pyspark
 # For generating a conda file for running python gpu and pyspark:
 # $ sh generate_conda_file.sh --gpu --pyspark
-# For generating a conda file for running python gpu and pyspark a particular version of spark:
-# $ sh generate_conda_file.sh --gpu --pyspark-version 2.4.0
 #
 
 # first check if conda is installed
@@ -32,7 +30,6 @@ pyspark="#"
 # flags to detect if both CPU and GPU are specified
 gpu_flag=false
 pyspark_flag=false
-pyspark_version=2.3.1
 
 while [ ! $# -eq 0 ]
 do
@@ -40,7 +37,6 @@ do
 		--help)
 			echo "Please specify --gpu to install with GPU-support and"
 			echo "--pyspark to install with pySpark support"
-			echo "--pyspark-version X.Y.Z to install version X.Y.Z of pySpark (default 2.3.1)"
 			exit
 			;;
 		--gpu)
@@ -53,17 +49,6 @@ do
 			pyspark=""
 			CONDA_FILE="conda_pyspark.yaml"
 			pyspark_flag=true
-			;;			
-		--pyspark-version)
-			pyspark=""
-			CONDA_FILE="conda_pyspark.yaml"
-			pyspark_flag=true
-			pyspark_version=$2
-			if ! [[ $pyspark_version =~ ^[0-9]+([.][0-9]+){2}$ ]]; then
-				echo "Inappropriate version of pyspark passed:" $pyspark_version
-				exit 1
-			fi
-			shift
 			;;			
 		*)
 			echo $"Usage: $0 invalid argument $1 please run with --help for more information."
@@ -97,7 +82,7 @@ dependencies:
 - python==3.6.7
 - numpy>=1.13.3
 - dask>=0.17.1
-${pyspark}- pyspark==${pyspark_version}
+${pyspark}- pyspark==2.3.1
 - pymongo>=3.6.1
 - ipykernel>=4.6.1
 - ${tensorflow}==1.12.0
