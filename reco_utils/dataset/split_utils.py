841c01278336a7a9dd93e260f9f3ccf34f331a60
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-02-19 11:14:50 +0000

    addressed @loomlike comment for shuffle

--- reco_utils/dataset/split_utils.py
+++ reco_utils/dataset/split_utils.py
@@ -135,19 +135,18 @@ def _check_min_rating_filter(filter_by, min_rating, col_user, col_item):
     return split_by_column, split_with_column
 
 
-def split_pandas_data_with_ratios(data, ratios, seed=1234, resample=False):
+def split_pandas_data_with_ratios(data, ratios, seed=1234, shuffle=False):
     """Helper function to split pandas DataFrame with given ratios
 
     Note:
         Implementation referenced from
-        https://stackoverflow.com/questions/38250710/how-to-split-data-into-3-sets-train
-        -validation-and-test
+        https://stackoverflow.com/questions/38250710/how-to-split-data-into-3-sets-train-validation-and-test
 
     Args:
         data (pd.DataFrame): Pandas data frame to be split.
         ratios (list of floats): list of ratios for split. The ratios have to sum to 1.
         seed (int): random seed.
-        resample (bool): whether data will be resampled when being split.
+        shuffle (bool): whether data will be shuffled when being split.
 
     Returns:
         list: List of pd.DataFrame splitted by the given specifications.
@@ -157,7 +156,7 @@ def split_pandas_data_with_ratios(data, ratios, seed=1234, resample=False):
 
     split_index = np.cumsum(ratios).tolist()[:-1]
 
-    if resample:
+    if shuffle:
         data = data.sample(frac=1, random_state=seed)
 
     splits = np.split(data, [round(x * len(data)) for x in split_index])
