a7d3b0d55bc90d3cfcc3d3ec38c154590a3b4a8a
Scott Graham <gramhagen@users.noreply.github.com>
2019-02-18 22:08:00 -0500

    adding tests and cleaning up vowpal wabbit wapper

--- reco_utils/recommender/vowpal_wabbit/vw.py
+++ reco_utils/recommender/vowpal_wabbit/vw.py
@@ -1,9 +1,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-"""This file provides a wrapper to run Vowpal Wabbit from the command line through python
-It is not recommended to use this approach in production, there are python bindings that can be installed from the repository or pip
-Or using the command line directly, this is merely to demonstrate vw usage in the example notebooks
+"""
+This file provides a wrapper to run Vowpal Wabbit from the command line through python.
+It is not recommended to use this approach in production, there are python bindings that can be installed from the
+repository or pip or the command line can be used. This is merely to demonstrate vw usage in the example notebooks.
 """
 
 import os
@@ -25,23 +26,25 @@ from reco_utils.common.constants import (
 class VW:
     """Vowpal Wabbit Class"""
 
-    def __init__(self,
-                 col_user=DEFAULT_USER_COL,
-                 col_item=DEFAULT_ITEM_COL,
-                 col_rating=DEFAULT_RATING_COL,
-                 col_timestamp=DEFAULT_TIMESTAMP_COL,
-                 col_prediction=PREDICTION_COL,
-                 **kwargs):
+    def __init__(
+        self,
+        col_user=DEFAULT_USER_COL,
+        col_item=DEFAULT_ITEM_COL,
+        col_rating=DEFAULT_RATING_COL,
+        col_timestamp=DEFAULT_TIMESTAMP_COL,
+        col_prediction=PREDICTION_COL,
+        **kwargs,
+    ):
         """Initialize model parameters
         Args:
         """
 
         # create temporary files
         self.tempdir = TemporaryDirectory()
-        self.train_file = os.path.join(self.tempdir.name, 'train.dat')
-        self.test_file = os.path.join(self.tempdir.name, 'train.dat')
-        self.model_file = os.path.join(self.tempdir.name, 'vw.model')
-        self.prediction_file = os.path.join(self.tempdir.name, 'prediciton.dat')
+        self.train_file = os.path.join(self.tempdir.name, "train.dat")
+        self.test_file = os.path.join(self.tempdir.name, "test.dat")
+        self.model_file = os.path.join(self.tempdir.name, "vw.model")
+        self.prediction_file = os.path.join(self.tempdir.name, "prediction.dat")
 
         # set DataFrame columns
         self.col_user = col_user
@@ -50,7 +53,7 @@ class VW:
         self.col_timestamp = col_timestamp
         self.col_prediction = col_prediction
 
-        self.logistic = 'logistic' in kwargs.values()
+        self.logistic = "logistic" in kwargs.values()
         self.train_params = self.parse_train_params(params=kwargs)
         self.test_params = self.parse_test_params(params=kwargs)
 
@@ -62,14 +65,20 @@ class VW:
         Returns:
             list[str]: vw command line parameters as list of strings
         """
-        cmd = ['vw']
+        cmd = ["vw"]
         for k, v in params.items():
-            if isinstance(v, bool) and v:
-                entry = f'--{k}'
+            if isinstance(v, bool):
+                if not v:
+                    continue
+                else:
+                    value = ""
             else:
-                hyphen = '-' if len(k) == 1 else '--'
-                entry = f'{hyphen}{k}={v}'
+                value = f"={v}"
+
+            hyphen = "-" if len(k) == 1 else "--"
+            entry = f"{hyphen}{k}{value}"
             cmd.append(entry)
+        return cmd
 
     def parse_train_params(self, params):
         """Parse input hyper-parameters to build vw train commands"""
@@ -78,15 +87,29 @@ class VW:
         train_params = params.copy()
 
         # remove options that are handled internally, not supported, or test only parameters
-        for option in ['data', 'final_regressor', 'invert_hash', 'readable_model'
-                       't', 'testonly', 'i', 'initial_regressor', 'link']:
-            del train_params[option]
+        invalid = [
+            "data",
+            "final_regressor",
+            "invert_hash",
+            "readable_model",
+            "t",
+            "testonly",
+            "i",
+            "initial_regressor",
+            "link",
+        ]
 
-        train_params.update({
-            'd': self.train_file,
-            'f': self.model_file,
-            'quiet': params.get('quiet', True)
-        })
+        for option in invalid:
+            if option in train_params:
+                del train_params[option]
+
+        train_params.update(
+            {
+                "d": self.train_file,
+                "f": self.model_file,
+                "quiet": params.get("quiet", True),
+            }
+        )
         return self.to_vw_cmd(params=train_params)
 
     def parse_test_params(self, params):
@@ -96,19 +119,51 @@ class VW:
         test_params = params.copy()
 
         # remove options that are handled internally, ot supported or train only parameters
-        for option in ['data', 'f', 'final_regressor', 'initial_regressor', 'testonly', 'invert_hash', 'readable_model',
-                       'b', 'bit_precision', 'holdout_off', 'c', 'cache', 'k', 'kill_cache',
-                       'l', 'learning_rate', 'l1', 'l2', 'initial_t', 'power_t', 'decay_learning_rate',
-                       'q', 'quadratic', 'cubic', 'i', 'interactions', 'rank', 'lrq', 'lrqdropout', 'oaa']:
-            del params[option]
+        invalid = [
+            "data",
+            "f",
+            "final_regressor",
+            "initial_regressor",
+            "test_only",
+            "invert_hash",
+            "readable_model",
+            "b",
+            "bit_precision",
+            "holdout_off",
+            "c",
+            "cache",
+            "k",
+            "kill_cache",
+            "l",
+            "learning_rate",
+            "l1",
+            "l2",
+            "initial_t",
+            "power_t",
+            "decay_learning_rate",
+            "q",
+            "quadratic",
+            "cubic",
+            "i",
+            "interactions",
+            "rank",
+            "lrq",
+            "lrqdropout",
+            "oaa",
+        ]
+        for option in invalid:
+            if option in test_params:
+                del test_params[option]
 
-        test_params.update({
-            'd': self.test_file,
-            'i': self.model_file,
-            'quiet': params.get('quiet', True),
-            't': True,
-            'p': self.prediction_file
-        })
+        test_params.update(
+            {
+                "d": self.test_file,
+                "i": self.model_file,
+                "quiet": params.get("quiet", True),
+                "p": self.prediction_file,
+                "t": True,
+            }
+        )
         return self.to_vw_cmd(params=test_params)
 
     def to_vw_file(self, df, train=True):
@@ -119,32 +174,36 @@ class VW:
         """
 
         output = self.train_file if train else self.test_file
-        with open(output, 'w') as f:
+        with open(output, "w") as f:
             # extract columns and create a new dataframe
             tmp = df[[self.col_rating, self.col_user, self.col_item]].reset_index()
 
             if train:
                 # we need to reset the rating type to an integer to simplify the vw formatting
-                tmp[self.col_rating] = tmp[self.col_rating].astype('int64')
+                tmp[self.col_rating] = tmp[self.col_rating].astype("int64")
 
                 # convert rating to binary value
                 if self.logistic:
                     max_value = tmp[self.col_rating].max()
-                    tmp[self.col_rating] = tmp[self.col_rating].apply(lambda x: 2 * round(x / max_value) - 1)
+                    tmp[self.col_rating] = tmp[self.col_rating].apply(
+                        lambda x: 2 * round(x / max_value) - 1
+                    )
             else:
-                tmp[self.col_rating] = ''
+                tmp[self.col_rating] = ""
 
             # convert each row to VW input format (https://github.com/VowpalWabbit/vowpal_wabbit/wiki/Input-format)
             # [label] [tag]|[user namespace] [user id feature] |[item namespace] [movie id feature]
             # label is the true rating, tag is a unique id for the example just used to link predictions to truth
             # user and item namespaces separate features to support interaction features through command line options
             for _, row in tmp.iterrows():
-                f.write('{rating} {index}|user {userID} |item {itemID}\n'.format(
-                    rating=row[self.col_rating],
-                    index=row['index'],
-                    userID=row[self.col_user],
-                    itemID=row[self.col_item]
-                ))
+                f.write(
+                    "{rating} {index}|user {userID} |item {itemID}\n".format(
+                        rating=row[self.col_rating],
+                        index=row["index"],
+                        userID=row[self.col_user],
+                        itemID=row[self.col_item],
+                    )
+                )
 
     def fit(self, df):
         """Train model
@@ -171,10 +230,14 @@ class VW:
         run(self.test_params, check=True)
 
         # read predictions
-        return df.join(pd.read_csv(self.prediction_file,
-                                   delim_whitespace=True,
-                                   names=[self.col_prediction],
-                                   index_col=1))
+        return df.join(
+            pd.read_csv(
+                self.prediction_file,
+                delim_whitespace=True,
+                names=[self.col_prediction],
+                index_col=1,
+            )
+        )
 
     def __del__(self):
         self.tempdir.cleanup()
