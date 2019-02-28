c0c99c1055d002c3dea013c3f1e3ae8fa8cb2528
yueguoguo <yueguoguo1024@gmail.com>
2019-02-28 12:08:03 +0000

    TEST: revert to previous in the staging branch

--- tests/conftest.py
+++ tests/conftest.py
@@ -183,10 +183,7 @@ def notebooks():
         "vowpal_wabbit_deep_dive": os.path.join(
             folder_notebooks, "02_model", "vowpal_wabbit_deep_dive.ipynb"
         ),
-        "evaluation": os.path.join(folder_notebooks, "03_evaluate", "evaluation.ipynb"),
-        "spark_hypertune": os.path.join(
-            folder_notebooks,
-            "04_model_select_and_optimize", "hypertune_spark_deep_dive.ipynb"),
+        "evaluation": os.path.join(folder_notebooks, "03_evaluate", "evaluation.ipynb")
     }
     return paths
 
