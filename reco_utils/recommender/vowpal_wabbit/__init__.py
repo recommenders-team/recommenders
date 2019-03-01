e7ba3d1ff246b136a5bcde945fb14633ba349a66
Scott Graham <gramhagen@users.noreply.github.com>
2019-03-01 09:15:15 -0500

    adding vowpal wabbit wrapper

---
+++ reco_utils/recommender/vowpal_wabbit/__init__.py
@@ -0,0 +1,8 @@
+__title__ = "Microsoft Recommenders"
+__version__ = "2019.02"
+__author__ = "RecoDev Team at Microsoft"
+__license__ = "MIT"
+__copyright__ = "Copyright 2018-present Microsoft Corporation"
+
+# Version synonym
+VERSION = __version__
