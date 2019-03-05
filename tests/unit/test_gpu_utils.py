1ccdb6d6f657332dcab71ff1e71c00f98b1b86b5
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-03-05 13:38:16 +0000

    addressed comment of gpu utils and added tests

---
+++ tests/unit/test_gpu_utils.py
@@ -0,0 +1,28 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import sys
+import pytest
+from reco_utils.common.gpu_utils import get_number_gpus, clear_memory_all_gpus, get_cuda_version, get_cudnn_version
+
+
+@pytest.mark.gpu
+def test_get_number_gpus()
+    assert get_number_gpus() >= 1
+
+
+@pytest.mark.gpu
+@pytest.mark.skip(reason="TODO: Implement this")
+def test_clear_memory_all_gpus()
+    pass
+
+
+@pytest.mark.gpu
+@pytest.mark.skipif(sys.platform == 'win32', reason="Not implemented on Windows")
+def test_get_cuda_version():
+    assert get_cuda_version() > "9.0.0"
+
+
+@pytest.mark.gpu
+def test_get_cudnn_version():
+    assert get_cudnn_version() > "7.0.0" 
