7afbf6d0f97e129c1a7a05f2024ee20111ebd7c3
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-03-07 15:37:40 +0000

    added case where timer has not been stopped, but user is calling interval

--- reco_utils/common/timer.py
+++ reco_utils/common/timer.py
@@ -10,17 +10,15 @@ class Timer(object):
     Original code: https://github.com/miguelgfierro/codebase
     
     Examples:
-        >>> big_num = 1000
+        >>> import time
         >>> t = Timer()
         >>> t.start()
-        >>> r = 0
-        >>> a = [r+i for i in range(big_num)]
+        >>> time.sleep(1)
         >>> t.stop()
         >>> t.interval < 1
         True
-        >>> r = 0
         >>> with Timer() as t:
-        ...   a = [r+i for i in range(big_num)]
+        ...   time.sleep(1)
         >>> t.interval < 1
         True
         >>> "Time elapsed {}".format(t) #doctest: +ELLIPSIS
@@ -29,7 +27,8 @@ class Timer(object):
 
     def __init__(self):
         self._timer = default_timer
-        self.interval = 0
+        self._interval = 0
+        self.running = False
 
     def __enter__(self):
         self.start()
@@ -39,18 +38,27 @@ class Timer(object):
         self.stop()
 
     def __str__(self):
-        return str(timedelta(seconds=self.interval))
+        return str(timedelta(seconds=self._interval))
 
     def start(self):
         """Start the timer."""
         self.init = self._timer()
+        self.running = True
 
     def stop(self):
         """Stop the timer. Calculate the interval in seconds."""
         self.end = self._timer()
         try:
-            self.interval = self.end - self.init
+            self._interval = self.end - self.init
+            self.running = False
         except AttributeError:
             raise ValueError(
                 "Timer has not been initialized: use start() or the contextual form with Timer() as t:"
             )
+
+    @property
+    def interval(self):
+        if self.running:
+            raise ValueError("Timer has not been stopped, please use stop().")
+        else:
+            return self._interval
