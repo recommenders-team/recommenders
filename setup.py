e736b08e5448d564e55baec2e29d3fe6c0f72607
Daniel Ciborowski <dciborow@microsoft.com>
2019-03-12 13:15:01 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='RecoUtils',
+      version='1.0',
+      description='Reco Utils',
+      author='',
+      author_email='',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['reco_utils'],
+     )
