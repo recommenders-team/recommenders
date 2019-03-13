71a16412e1bb31e4ab43e05e4a33f46bfc2b9b16
Andreas Argyriou <anargyri@microsoft.com>
2019-03-13 19:51:06 +0000

    TUNING: add script and notebook for NNI tuning of SVD

---
+++ reco_utils/nni/svd_training.py
@@ -0,0 +1,128 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+import sys
+
+sys.path.append("../../")
+
+import argparse
+import logging
+import nni
+import os
+import pandas as pd
+import surprise
+
+from reco_utils.evaluation.python_evaluation import *
+from reco_utils.recommender.surprise.surprise_utils import compute_rating_predictions, compute_ranking_predictions
+
+logger = logging.getLogger("surprise_svd")
+
+
+def svd_training(params):
+    """
+    Train Surprise SVD using the given hyper-parameters
+    """
+    logger.debug("Start training...")
+    train_data = pd.read_pickle(path=os.path.join(params['datastore'], params['train_datapath']))
+    validation_data = pd.read_pickle(path=os.path.join(params['datastore'], params['validation_datapath']))
+
+    svd = surprise.SVD(random_state=params['random_state'], n_epochs=params['epochs'], verbose=params['verbose'],
+                       biased=params['biased'], n_factors=params['n_factors'], init_mean=params['init_mean'],
+                       init_std_dev=params['init_std_dev'], lr_all=params['lr_all'], reg_all=params['reg_all'],
+                       lr_bu=params['lr_bu'], lr_bi=params['lr_bi'], lr_pu=params['lr_pu'], lr_qi=params['lr_qi'],
+                       reg_bu=params['reg_bu'], reg_bi=params['reg_bi'], reg_pu=params['reg_pu'],
+                       reg_qi=params['reg_qi'])
+
+    train_set = surprise.Dataset.load_from_df(train_data, reader=surprise.Reader(params['surprise_reader'])) \
+        .build_full_trainset()
+    svd.fit(train_set)
+
+    logger.debug("Evaluating...")
+
+    rating_metrics = params['rating_metrics']
+    if len(rating_metrics) > 0:
+        predictions = compute_rating_predictions(svd, validation_data, usercol=params['usercol'],
+                                                 itemcol=params['itemcol'])
+        for metric in rating_metrics:
+            result = eval(metric)(validation_data, predictions)
+            logger.debug(metric, result)
+            if metric == params['primary_metric']:
+                nni.report_final_result(metric)
+            else:
+                nni.report_intermediate_result(metric)
+
+    ranking_metrics = params['ranking_metrics']
+    if len(ranking_metrics) > 0:
+        all_predictions = compute_ranking_predictions(svd, train_data, usercol=params['usercol'],
+                                                      itemcol=params['itemcol'],
+                                                      recommend_seen=params['recommend_seen'])
+        k = params['k']
+        for metric in ranking_metrics:
+            result = eval(metric)(validation_data, all_predictions, col_prediction='prediction', k=k)
+            logger.debug("{}@{}".format(metric, k), result)
+            if metric == params['primary_metric']:
+                nni.report_final_result(metric)
+            else:
+                nni.report_intermediate_result(metric)
+
+    if len(ranking_metrics) == 0 and len(rating_metrics) == 0:
+        raise ValueError("No metrics were specified.")
+
+    return svd
+
+
+def get_params():
+    parser = argparse.ArgumentParser()
+    # Data path
+    parser.add_argument('--datastore', type=str, dest='datastore', help="Datastore path")
+    parser.add_argument('--train-datapath', type=str, dest='train_datapath')
+    parser.add_argument('--validation-datapath', type=str, dest='validation_datapath')
+    parser.add_argument('--output_dir', type=str, help='output directory')
+    parser.add_argument('--surprise-reader', type=str, dest='surprise_reader')
+    parser.add_argument('--usercol', type=str, dest='usercol', default='userID')
+    parser.add_argument('--itemcol', type=str, dest='itemcol', default='itemID')
+    # Metrics
+    parser.add_argument('--rating-metrics', type=str, nargs='*', dest='rating_metrics', default=[])
+    parser.add_argument('--ranking-metrics', type=str, nargs='*', dest='ranking_metrics', default=[])
+    parser.add_argument('--k', type=int, dest='k', default=None)
+    parser.add_argument('--recommend-seen', dest='recommend_seen', action='store_true')
+    # Training parameters
+    parser.add_argument('--random-state', type=int, dest='random_state', default=0)
+    parser.add_argument('--verbose', dest='verbose', action='store_true')
+    parser.add_argument('--epochs', type=int, dest='epochs', default=30)
+    parser.add_argument('--biased', dest='biased', action='store_true')
+    parser.add_argument('primary_metric', dest='primary_metric', default='rmse')
+    # Hyperparameters to be tuned
+    parser.add_argument('--n_factors', type=int, dest='n_factors', default=100)
+    parser.add_argument('--init_mean', type=float, dest='init_mean', default=0.0)
+    parser.add_argument('--init_std_dev', type=float, dest='init_std_dev', default=0.1)
+    parser.add_argument('--lr_all', type=float, dest='lr_all', default=0.005)
+    parser.add_argument('--reg_all', type=float, dest='reg_all', default=0.02)
+    parser.add_argument('--lr_bu', type=float, dest='lr_bu', default=None)
+    parser.add_argument('--lr_bi', type=float, dest='lr_bi', default=None)
+    parser.add_argument('--lr_pu', type=float, dest='lr_pu', default=None)
+    parser.add_argument('--lr_qi', type=float, dest='lr_qi', default=None)
+    parser.add_argument('--reg_bu', type=float, dest='reg_bu', default=None)
+    parser.add_argument('--reg_bi', type=float, dest='reg_bi', default=None)
+    parser.add_argument('--reg_pu', type=float, dest='reg_pu', default=None)
+    parser.add_argument('--reg_qi', type=float, dest='reg_qi', default=None)
+
+    args = parser.parse_args()
+    return args
+
+
+def main(params):
+    logger.debug("Args:", str(params), sep='\n')
+    logger.debug('Number of epochs', params["epochs"])
+
+    svd = svd_training(params)
+    # Save SVD model to the output directory for later use
+    os.makedirs(params["output_dir"], exist_ok=True)
+    surprise.dump.dump(os.path.join(params["output_dir"], 'model.dump'), algo=svd)
+
+
+if __name__ == "__main__":
+    tuner_params = nni.get_next_parameter()
+    logger.debug(tuner_params)
+    params = vars(get_params())
+    params.update(tuner_params)
+    main(params)
