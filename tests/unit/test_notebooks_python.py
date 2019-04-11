af76b721917a5df4290f9b095190c047e6acf350
Andreas Argyriou <anargyri@microsoft.com>
2019-04-11 14:50:34 +0000

    NNI: move tests from unit to integration; rewrite timing in NNI utils

--- tests/unit/test_notebooks_python.py
+++ tests/unit/test_notebooks_python.py
@@ -1,14 +1,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import os
 import pytest
 import papermill as pm
-import shutil
-import subprocess
-import sys
 
-from reco_utils.nni.nni_utils import check_stopped
 from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
 
 
@@ -50,7 +45,7 @@ def test_baseline_deep_dive_runs(notebooks):
 def test_surprise_deep_dive_runs(notebooks):
     notebook_path = notebooks["surprise_svd_deep_dive"]
     pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
-    
+
 
 @pytest.mark.notebooks
 def test_vw_deep_dive_runs(notebooks):
@@ -67,20 +62,4 @@ def test_lightgbm(notebooks):
                                         NUM_OF_TREES=10,
                                         TREE_LEARNING_RATE=0.15,
                                         EARLY_STOPPING_ROUNDS=20,
-                                        METRIC="auc"),)
-
-
-@pytest.mark.notebooks
-def test_nni_tuning(notebooks, tmp):
-    notebook_path = notebooks["nni_tuning_svd"]
-    # First stop NNI in case it is running
-    subprocess.run([sys.prefix + '/bin/nnictl', 'stop'])
-    check_stopped()
-    pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME,
-                        parameters=dict(MOVIELENS_DATA_SIZE="100k",
-                                        SURPRISE_READER="ml-100k",
-                                        TMP_DIR=tmp,
-                                        MAX_TRIAL_NUM=10))
-    # Clean up logs, saved models etc. under the NNI path
-    nni_path = pm.read_notebook(OUTPUT_NOTEBOOK).data["nni_path"]
-    shutil.rmtree(nni_path, ignore_errors=True)
+                                        METRIC="auc"), )
