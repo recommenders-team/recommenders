47ef7f5d204d79e9f2352676120c351be85e3f08
Jeremy Reynolds <jeremy.reynolds@microsoft.com>
2019-04-16 08:13:10 -0600

    use logger instead of verbose

--- reco_utils/azureml/aks_utils.py
+++ reco_utils/azureml/aks_utils.py
@@ -1,6 +1,9 @@
 from math import ceil, floor
+import logging
 
-def qps_to_replicas(target_qps, processing_time, max_qp_replica=1, target_utilization=0.7, verbose=True):
+logger = logging.getLogger(__name__)
+
+def qps_to_replicas(target_qps, processing_time, max_qp_replica=1, target_utilization=0.7):
     """Provide a rough estimate of the number of replicas to support a given load (queries per second)
     
     Args:
@@ -15,11 +18,10 @@ def qps_to_replicas(target_qps, processing_time, max_qp_replica=1, target_utiliz
     """
     concurrent_queries = target_qps * processing_time / target_utilization
     replicas = ceil(concurrent_queries / max_qp_replica)
-    if verbose:
-        print('Approximately {} replicas are estimated to support {} queries per second.'.format(replicas, target_qps))
+    logger.info('Approximately {} replicas are estimated to support {} queries per second.'.format(replicas, target_qps))
     return replicas
     
-def replicas_to_qps(num_replicas, processing_time, max_qp_replica=1, target_utilization=0.7, verbose=True):
+def replicas_to_qps(num_replicas, processing_time, max_qp_replica=1, target_utilization=0.7):
     """Provide a rough estimate of the queries per second supported by a number of replicas
     
     Args:
@@ -28,17 +30,16 @@ def replicas_to_qps(num_replicas, processing_time, max_qp_replica=1, target_util
         max_qp_replica (int): maximum number of concurrent queries per replica
         target_utilization (float): proportion of CPU utilization you think is ideal
         verbose (bool): Print out a message before exiting
-        
+
     Returns:
         qps: queries per second supported by the number of replicas
     """
     qps = floor(num_replicas*max_qp_replica*target_utilization/processing_time)
-    if verbose:
-        print('Approximately {} queries per second are supported by {} replicas.'.format(qps, num_replicas))
+    logger.info('Approximately {} queries per second are supported by {} replicas.'.format(qps, num_replicas))
     return qps
 
 
-def total_cores_to_replicas(n_cores, cpu_cores_per_replica=0.1, overhead=0.1, verbose=True):
+def total_cores_to_replicas(n_cores, cpu_cores_per_replica=0.1, overhead=0.1):
     """Provide a rough estimate of the number of replicas supported by a particular number of cores.
 
     Args:
@@ -52,6 +53,5 @@ def total_cores_to_replicas(n_cores, cpu_cores_per_replica=0.1, overhead=0.1, ve
         replicas: Total number of replicas supported by n_cores
     """
     replicas = floor((1 - overhead)*n_cores/(cpu_cores_per_replica))
-    if verbose:
-        print('Approximately {} replicas are supported by {} cores.'.format(replicas, n_cores))
+    logger.info('Approximately {} replicas are supported by {} cores.'.format(replicas, n_cores))
     return replicas
