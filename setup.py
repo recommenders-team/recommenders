7b0043a2bf615c2289543427b1b80ed328daf78b
Daniel Ciborowski <dciborow@microsoft.com>
2019-05-07 18:53:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='RecoUtils',
-      version='2019.02',
+      version=VERSION,
       description='Reco Utils',
       author='Microsoft',
       packages=['reco_utils','reco_utils.common','reco_utils.dataset','reco_utils.evaluation','reco_utils.recommender'],
