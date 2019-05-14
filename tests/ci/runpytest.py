e773a0cad3255106e8668e307ae10b20043483e7
bethz <6098674+bethz@users.noreply.github.com>
2019-05-14 08:55:20 -0400

    add all unit tests

--- tests/ci/runpytest.py
+++ tests/ci/runpytest.py
@@ -6,7 +6,7 @@ print('before subprocess.run')
 #subprocess.run(["python", "-m", "pytest","tests/unit","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
 #subprocess.run(["pytest","tests/unit/test_python_utils.py","-m","not notebooks and not spark and gpu", "--junitxml=reports/test-unit.xml","-s", "|","./logs/pytest.log"])
 #subprocess.run(["pytest","tests/unit/test_dataset.py","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
-subprocess.run(["pytest","tests/unit/test_dataset.py","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
+subprocess.run(["pytest","tests/unit","-m","not notebooks and not spark and not gpu", "--junitxml=reports/test-unit.xml"])
 
 import os
 print("os.listdir files", os.listdir("."))
