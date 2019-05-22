35f63ff8f376cdd58b5493ae69fc48785843b40f
yueguoguo <yueguoguo1024@gmail.com>
2019-05-22 01:54:36 +0000

    DATA: update docstring

--- reco_utils/dataset/pandas_df_utils.py
+++ reco_utils/dataset/pandas_df_utils.py
@@ -116,11 +116,11 @@ class LibffmConverter(object):
         >>> df_out = converter.transform(df_feature)
         >>> df_out
             rating field1 field2   field3 field4
-        0       1  1:1:1  2:2:3  3:3:1.0  4:4:1
-        1       0  1:2:1  2:2:4  3:3:2.0  4:5:1
-        2       0  1:3:1  2:2:5  3:3:3.0  4:6:1
-        3       1  1:3:1  2:2:6  3:3:4.0  4:7:1
-        4       1  1:3:1  2:2:7  3:3:5.0  4:8:1
+        0       1  1:1:1  2:4:3  3:5:1.0  4:4:1
+        1       0  1:2:1  2:4:4  3:5:2.0  4:5:1
+        2       0  1:3:1  2:4:5  3:5:3.0  4:6:1
+        3       1  1:3:1  2:4:6  3:5:4.0  4:7:1
+        4       1  1:3:1  2:4:7  3:5:5.0  4:8:1
 
     Args:
         filepath (str): path to save the converted data.
