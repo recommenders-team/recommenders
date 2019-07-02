ec1ce832b3aaaa2f8889b92d33a64d75981527e6
Scott Graham <gramhagen@users.noreply.github.com>
2019-07-02 08:44:38 -0400

    black formatting

--- reco_utils/setup.py
+++ reco_utils/setup.py
@@ -5,34 +5,34 @@ from setuptools import setup, find_packages
 from os import chdir, path
 
 
-VERSION = __import__('__init__').VERSION
+VERSION = __import__("__init__").VERSION
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 chdir(here)
 setup(
-    name='reco_utils',
+    name="reco_utils",
     version=VERSION,
-    description='Recommender System Utilities',
+    description="Recommender System Utilities",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/microsoft/recommenders',
-    author='RecoDev Team at Microsoft',
-    author_email='RecoDevTeam@service.microsoft.com',
+    long_description_content_type="text/markdown",
+    url="https://github.com/microsoft/recommenders",
+    author="RecoDev Team at Microsoft",
+    author_email="RecoDevTeam@service.microsoft.com",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
     ],
-    keywords='recommendations recommenders recommender system engine machine learning python spark gpu',
+    keywords="recommendations recommenders recommender system engine machine learning python spark gpu",
     packages=find_packages(),
-    python_requires='>=3.6, <4',
+    python_requires=">=3.6, <4",
 )
