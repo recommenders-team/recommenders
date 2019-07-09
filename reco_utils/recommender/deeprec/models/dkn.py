a60d81a6768e7d022f388bbbb57e72cc71ebe88c
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-07-09 09:04:35 +0100

    :bug: it's models not model :boom:

--- reco_utils/recommender/deeprec/models/dkn.py
+++ reco_utils/recommender/deeprec/models/dkn.py
@@ -4,7 +4,7 @@
 import numpy as np
 import tensorflow as tf
 
-from reco_utils.recommender.deeprec.model.base_model import BaseModel
+from reco_utils.recommender.deeprec.models.base_model import BaseModel
 
 __all__ = ["DKN"]
 
