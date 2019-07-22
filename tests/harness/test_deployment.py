d6d815f150e7d7ac44f49473949289ebc4bcd13a
Daniel Ciborowski <dciborow@microsoft.com>
2019-07-22 13:47:25 -0400

    Update test_deployment.py

--- tests/harness/test_deployment.py
+++ tests/harness/test_deployment.py
@@ -159,42 +159,42 @@ def test_harness():
           recs.withColumn("id", recs[userCol].cast("string")).select("id", "recommendations." + itemCol) \
               .write.format("com.microsoft.azure.cosmosdb.spark").mode('overwrite').options(**writeConfig).save()
 
-      score_sparkml = """
+    score_sparkml = """
 
+import json
+def init(local=False):
+    global client, collection
+    try:
+      # Query them in SQL
+      import pydocumentdb.document_client as document_client
+
+      MASTER_KEY = '{key}'
+      HOST = '{endpoint}'
+      DATABASE_ID = "{database}"
+      COLLECTION_ID = "{collection}"
+      database_link = 'dbs/' + DATABASE_ID
+      collection_link = database_link + '/colls/' + COLLECTION_ID
+
+      client = document_client.DocumentClient(HOST, {'masterKey': MASTER_KEY})
+      collection = client.ReadCollection(collection_link=collection_link)
+    except Exception as e:
+      collection = e
+def run(input_json):      
+
+    try:
       import json
-      def init(local=False):
-          global client, collection
-          try:
-            # Query them in SQL
-            import pydocumentdb.document_client as document_client
 
-            MASTER_KEY = '{key}'
-            HOST = '{endpoint}'
-            DATABASE_ID = "{database}"
-            COLLECTION_ID = "{collection}"
-            database_link = 'dbs/' + DATABASE_ID
-            collection_link = database_link + '/colls/' + COLLECTION_ID
+      id = json.loads(json.loads(input_json)[0])['id']
+      query = {'query': 'SELECT * FROM c WHERE c.id = "' + str(id) +'"' } #+ str(id)
 
-            client = document_client.DocumentClient(HOST, {'masterKey': MASTER_KEY})
-            collection = client.ReadCollection(collection_link=collection_link)
-          except Exception as e:
-            collection = e
-      def run(input_json):      
+      options = {'partitionKey':str(id)}
+      document_link = 'dbs/{DOCUMENTDB_DATABASE}/colls/{DOCUMENTDB_COLLECTION}/docs/{0}'.format(id)
+      result = client.ReadDocument(document_link, options);
 
-          try:
-            import json
-
-            id = json.loads(json.loads(input_json)[0])['id']
-            query = {'query': 'SELECT * FROM c WHERE c.id = "' + str(id) +'"' } #+ str(id)
-
-            options = {'partitionKey':str(id)}
-            document_link = 'dbs/{DOCUMENTDB_DATABASE}/colls/{DOCUMENTDB_COLLECTION}/docs/{0}'.format(id)
-            result = client.ReadDocument(document_link, options);
-
-          except Exception as e:
-              result = str(e)
-          return json.dumps(str(result)) #json.dumps({{"result":result}})
-      """
+    except Exception as e:
+        result = str(e)
+    return json.dumps(str(result)) #json.dumps({{"result":result}})
+    """
 
       with open(secrets_path) as json_data:
           writeConfig = json.load(json_data)
