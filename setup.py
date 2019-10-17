cc018a15ac058f7f63fdf177a0101fb196d0af60
Scott Graham <5720537+gramhagen@users.noreply.github.com>
2019-10-17 11:39:27 -0400

    moving setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from setuptools import setup, find_packages
+from setuptools import setup
 from os import chdir, path
 
 chdir(path.abspath(path.dirname(__file__)))
-VERSION = __import__("__init__").VERSION
+VERSION = __import__("reco_utils.__init__").VERSION
 
 # Get the long description from the README file
-with open("README.md", encoding="utf-8") as f:
-    long_description = f.read()
+with open(path.join("reco_utils", "README.md"), encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(
     name="reco_utils",
     version=VERSION,
     description="Recommender System Utilities",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/microsoft/recommenders",
     author="RecoDev Team at Microsoft",
@@ -30,6 +30,6 @@ setup(
         "Programming Language :: Python :: 3.6",
     ],
     keywords="recommendations recommenders recommender system engine machine learning python spark gpu",
-    packages=find_packages(),
+    packages=["reco_utils"],
     python_requires=">=3.6, <4",
 )
