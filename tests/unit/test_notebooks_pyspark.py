2c120164e7136499cf2de8eacec8b5b47e5dea06
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-11-27 18:01:40 +0000

    speeding up tests

--- tests/unit/test_notebooks_pyspark.py
+++ tests/unit/test_notebooks_pyspark.py
@@ -9,6 +9,9 @@ from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
 
 @pytest.mark.notebooks
 @pytest.mark.spark
+@pytest.mark.skipif(
+    sys.platform == "win32", reason="Takes 1087.56s in Windows, while in Linux 52.51s"
+)
 def test_als_pyspark_runs(notebooks):
     notebook_path = notebooks["als_pyspark"]
     pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
@@ -23,6 +26,9 @@ def test_data_split_runs(notebooks):
 
 @pytest.mark.notebooks
 @pytest.mark.spark
+@pytest.mark.skipif(
+    sys.platform == "win32", reason="Takes 2764.50s in Windows, while in Linux 124.35s"
+)
 def test_als_deep_dive_runs(notebooks):
     notebook_path = notebooks["als_deep_dive"]
     pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
@@ -30,6 +36,9 @@ def test_als_deep_dive_runs(notebooks):
 
 @pytest.mark.notebooks
 @pytest.mark.spark
+@pytest.mark.skipif(
+    sys.platform == "win32", reason="Takes 583.75s in Windows, while in Linux 71.77s"
+)
 def test_evaluation_runs(notebooks):
     notebook_path = notebooks["evaluation"]
     pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
@@ -37,6 +46,9 @@ def test_evaluation_runs(notebooks):
 
 @pytest.mark.notebooks
 @pytest.mark.spark
+@pytest.mark.skipif(
+    sys.platform == "win32", reason="Takes 2409.69s in Windows, while in Linux 138.30s"
+)
 def test_spark_tuning(notebooks):
     notebook_path = notebooks["spark_tuning"]
     pm.execute_notebook(
@@ -48,23 +60,19 @@ def test_spark_tuning(notebooks):
             NUMBER_ITERATIONS=3,
             SUBSET_RATIO=0.5,
             RANK=[5, 5],
-            REG=[0.1, 0.01]
-        )
+            REG=[0.1, 0.01],
+        ),
     )
 
 
 @pytest.mark.notebooks
 @pytest.mark.spark
-@pytest.mark.skipif(sys.platform == 'win32', reason="Not implemented on Windows")
+@pytest.mark.skipif(sys.platform == "win32", reason="Not implemented on Windows")
 def test_mmlspark_lightgbm_criteo_runs(notebooks):
     notebook_path = notebooks["mmlspark_lightgbm_criteo"]
     pm.execute_notebook(
         notebook_path,
         OUTPUT_NOTEBOOK,
         kernel_name=KERNEL_NAME,
-        parameters=dict(
-            DATA_SIZE="sample",
-            NUM_ITERATIONS=10,
-            EARLY_STOPPING_ROUND=2,
-        )
+        parameters=dict(DATA_SIZE="sample", NUM_ITERATIONS=10, EARLY_STOPPING_ROUND=2),
     )
