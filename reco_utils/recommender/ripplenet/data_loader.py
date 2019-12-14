19e5a94d76e4c36f715e686586b4f6b28d525793
almudenasanz <so.almudena@gmail.com>
2019-12-14 13:01:42 +0100

    added docu

--- reco_utils/recommender/ripplenet/data_loader.py
+++ reco_utils/recommender/ripplenet/data_loader.py
@@ -6,69 +6,43 @@ import os
 import numpy as np
 
 
-def load_data(ratings_final, kg_final, n_hop, n_memory):
-    train_data, eval_data, test_data, user_history_dict = dataset_split(ratings_final)
-    n_entity, n_relation, kg = load_kg(kg_final)
-    ripple_set = get_ripple_set(kg, user_history_dict, n_hop, n_memory)
-    return train_data, eval_data, test_data, n_entity, n_relation, ripple_set
-
-def dataset_split(rating_np):
-    print('splitting dataset ...')
-
-    # train:eval:test = 6:2:2
-    eval_ratio = 0.2
-    test_ratio = 0.2
-    n_ratings = rating_np.shape[0]
-
-    eval_indices = np.random.choice(n_ratings, size=int(n_ratings * eval_ratio), replace=False)
-    left = set(range(n_ratings)) - set(eval_indices)
-    test_indices = np.random.choice(list(left), size=int(n_ratings * test_ratio), replace=False)
-    train_indices = list(left - set(test_indices))
-    # print(len(train_indices), len(eval_indices), len(test_indices))
-
-    # traverse training data, only keeping the users with positive ratings
-    user_history_dict = dict()
-    for i in train_indices:
-        user = rating_np.iloc[i][0]
-        item = rating_np.iloc[i][1]
-        rating = rating_np.iloc[i][2]
-        if rating == 1:
-            if user not in user_history_dict:
-                user_history_dict[user] = []
-            user_history_dict[user].append(item)
-
-    train_indices = [i for i in train_indices if rating_np.iloc[i][0] in user_history_dict]
-    eval_indices = [i for i in eval_indices if rating_np.iloc[i][0] in user_history_dict]
-    test_indices = [i for i in test_indices if rating_np.iloc[i][0] in user_history_dict]
-    # print(len(train_indices), len(eval_indices), len(test_indices))
-
-    train_data = rating_np.iloc[train_indices]
-    eval_data = rating_np.iloc[eval_indices]
-    test_data = rating_np.iloc[test_indices]
-
-    return train_data, eval_data, test_data, user_history_dict
-
-
 def load_kg(kg_final):
+    """Standarize indexes for items and entities
+
+    Args:
+        kg_final: dataframe, knowledge graph converted with columns head,
+         relation and tail, with internal entity IDs
+
+    Returns:
+        n_entity: int, number of entities in KG
+        n_relation: int, number of relations in KG
+        kg: KG in dictionary shape
+    """
     print('reading KG file ...')
 
     n_entity = len(set(kg_final.iloc[:, 0]) | set(kg_final.iloc[:, 2]))
     n_relation = len(set(kg_final.iloc[:, 1]))
 
-    kg = construct_kg(kg_final)
+    kg = collections.defaultdict(list)
+    for index, row in kg_final.iterrows():
+        kg[row["head"]].append((row["tail"], row["relation"]))
 
     return n_entity, n_relation, kg
 
 
-def construct_kg(kg_np):
-    print('constructing knowledge graph ...')
-    kg = collections.defaultdict(list)
-    for index, row in kg_np.iterrows():
-        kg[row["head"]].append((row["tail"], row["relation"]))
-    return kg
-
-
 def get_ripple_set(kg, user_history_dict, n_hop, n_memory):
+    """Build Ripple Set, dictionary for the related entities in the KG
+     given the paths of users, number of hops and memory
+
+    Args:
+        kg: KG in dictionary shape
+        user_history_dict: dataframe, train rating data with positive ratings
+        n_hop: int, maximum hops in the KG
+        n_memory: int, size of ripple set for each hop
+
+    Returns:
+        ripple_set: set of knowledge triples per user positive rating, from 0 until n_hop
+    """
     print('constructing ripple set ...')
 
     # user -> [(hop_0_heads, hop_0_relations, hop_0_tails), (hop_1_heads, hop_1_relations, hop_1_tails), ...]
