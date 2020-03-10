93291d7dbd18453c9153098bfc5a22468e66f88f
miguelgfierro <miguelgfierro@users.noreply.github.com>
2020-03-10 12:20:57 +0000

    fix setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from os import chdir, path
 
 chdir(path.abspath(path.dirname(__file__)))
@@ -30,6 +30,7 @@ setup(
         "Programming Language :: Python :: 3.6",
     ],
     keywords="recommendations recommenders recommender system engine machine learning python spark gpu",
-    packages=["reco_utils"],
+    package_dir={"": "reco_utils"},
+    packages=find_packages([where="reco_utils"], exclude=["azureml_designer_modules"])
     python_requires=">=3.6, <4",
 )
