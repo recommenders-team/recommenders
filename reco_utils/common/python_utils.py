Fix SAR normalization + add logloss eval prep function

reco_utils/common/python_utils.py
@@ -112,7 +112,30 @@ def binarize(a, threshold):
     """
     return np.where(
         a > threshold,
-        1.0,
+        1.0s,
         0.0
     )
 
+
+def rescale(data, new_min=0, new_max=1, data_min=None, data_max=None):
+    """
+    Rescale/normalize the data to be within the range [new_min, new_max]
+    If data_min and data_max are explicitly provided, they will be used
+    as the old min/max values instead of taken from the data.
+
+    Note: this is same as the scipy.MinMaxScaler with the exception that we can override
+          the min/max of the old scale.
+
+    Args:
+        data (np.array): 1d scores vector or 2d score matrix (users x items).
+        new_min (int|float): The minimum of the newly scaled data.
+        new_max (int|float): The maximum of the newly scaled data.
+        data_min (None|number): The minimum of the passed data [if omitted it will be inferred].
+        data_max (None|number): The maximum of the passed data [if omitted it will be inferred].
+
+    Returns:
+        np.array: The newly scaled/normalized data.
+    """
+    data_min = data.min() if data_min is None else data_min
+    data_max = data.max() if data_max is None else data_max
+    return (data - data_min) / (data_max - data_min) * (new_max - new_min) + new_min

reco_utils/recommender/sar/sar_singlenode.py
@@ -12,6 +12,7 @@ from reco_utils.common.python_utils import (
     lift,
     exponential_decay,
     get_top_k_scored_items,
+    rescale,
 )
 from reco_utils.common import constants
 
@@ -89,7 +90,6 @@ class SARSingleNode:
         # set flag to capture unity-rating user-affinity matrix for scaling scores
         self.normalize = normalize
         self.col_unity_rating = "_unity_rating"
-        self.unity_user_affinity = None
 
         # column for mapping user / item ids to internal indices
         self.col_item_id = "_indexed_items"
@@ -98,6 +98,10 @@ class SARSingleNode:
         # obtain all the users and items from both training and test data
         self.n_users = None
         self.n_items = None
+
+        # The min and max of the rating scale, obtained from the training data.
+        self.rating_scale_min = None
+        self.rating_scale_max = None
 
         # mapping for item to matrix element
         self.user2index = None
@@ -238,15 +242,11 @@ class SARSingleNode:
             lambda user: self.user2index.get(user, np.NaN)
         )
 
-        if self.normalize:
-            logger.info("Calculating normalization factors")
+        if self.normalize and self.time_decay_flag:
+            logger.info("Calculating normalized time-decayed affinities")
             temp_df[self.col_unity_rating] = 1.0
-            if self.time_decay_flag:
-                temp_df = self.compute_time_decay(
-                    df=temp_df, decay_column=self.col_unity_rating
-                )
-            self.unity_user_affinity = self.compute_affinity_matrix(
-                df=temp_df, rating_col=self.col_unity_rating
+            temp_df = self.compute_time_decay(
+                df=temp_df, decay_column=self.col_unity_rating
             )
 
         # affinity matrix
@@ -254,6 +254,9 @@ class SARSingleNode:
         self.user_affinity = self.compute_affinity_matrix(
             df=temp_df, rating_col=self.col_rating
         )
+
+        self.rating_scale_min = self.user_affinity.min()
+        self.rating_scale_max = self.user_affinity.max()
 
         # calculate item co-occurrence
         logger.info("Calculating item co-occurrence")
@@ -286,13 +289,12 @@ class SARSingleNode:
 
         logger.info("Done training")
 
-    def score(self, test, remove_seen=False, normalize=False):
+    def score(self, test, remove_seen=False):
         """Score all items for test users.
 
         Args:
             test (pd.DataFrame): user to test
             remove_seen (bool): flag to remove items seen in training from recommendation
-            normalize (bool): flag to normalize scores to be in the same scale as the original ratings
 
         Returns:
             np.ndarray: Value of interest of all items for the users.
@@ -316,24 +318,13 @@ class SARSingleNode:
         if isinstance(test_scores, sparse.spmatrix):
             test_scores = test_scores.toarray()
 
+        if self.normalize:
+            test_scores = rescale(test_scores, self.rating_scale_min, self.rating_scale_max, data_min=0)
+
         # remove items in the train set so recommended items are always novel
         if remove_seen:
             logger.info("Removing seen items")
             test_scores += self.user_affinity[user_ids, :] * -np.inf
-
-        if normalize:
-            if self.unity_user_affinity is None:
-                raise ValueError(
-                    "Cannot use normalize flag during scoring if it was not set at model instantiation"
-                )
-            else:
-                test_scores = np.array(
-                    np.divide(
-                        test_scores,
-                        self.unity_user_affinity[user_ids, :].dot(self.item_similarity),
-                    )
-                )
-                test_scores = np.where(np.isnan(test_scores), -np.inf, test_scores)
 
         return test_scores
 
@@ -439,7 +430,7 @@ class SARSingleNode:
         return df.replace(-np.inf, np.nan).dropna()
 
     def recommend_k_items(
-        self, test, top_k=10, sort_top_k=True, remove_seen=False, normalize=False
+        self, test, top_k=10, sort_top_k=True, remove_seen=False
     ):
         """Recommend top K items for all users which are in the test set
 
@@ -453,7 +444,7 @@ class SARSingleNode:
             pd.DataFrame: top k recommendation items for each user
         """
 
-        test_scores = self.score(test, remove_seen=remove_seen, normalize=normalize)
+        test_scores = self.score(test, remove_seen=remove_seen)
 
         top_items, top_scores = get_top_k_scored_items(
             scores=test_scores, top_k=top_k, sort_top_k=sort_top_k