df55c97323b343bfb20008de28f7325c8617f91b
Ubuntu <azureuser@wutaodsvmd16.rrjx4db4p1bunjdzc1bcnfnfng.bx.internal.cloudapp.net>
2020-11-08 03:03:14 +0000

    test mind update

--- tests/smoke/test_mind.py
+++ tests/smoke/test_mind.py
@@ -8,137 +8,32 @@ from reco_utils.dataset.mind import download_mind, extract_mind
 
 
 
-train_or_valid = ["train", "valid"]
 
-# zipsizes = {
-#     "small": {
-#         "train": 52952752,
-#         "valid": 30945572
-#     }
-# }
+@pytest.mark.parametrize("url, content_length, etag", 
+    [("https://recodatasets.blob.core.windows.net/newsrec/MINDdemo_train.zip",
+    "17372879", "0x8D82C63E386D09C"),
+    ("https://recodatasets.blob.core.windows.net/newsrec/MINDdemo_dev.zip",
+    "10080022", "0x8D82C6434EC3CEE"),
+    ("https://recodatasets.blob.core.windows.net/newsrec/MINDdemo_utils.zip", 
+    "97292694", "0x8D87F362FF7FB26"),
+    ("https://mind201910small.blob.core.windows.net/release/MINDsmall_train.zip",
+    "52952752","0x8D834F2EB31BDEC"),
+    ("https://mind201910small.blob.core.windows.net/release/MINDsmall_dev.zip",
+    "30945572","0x8D834F2EBA8D865"),
+    ("https://mind201910small.blob.core.windows.net/release/MINDsmall_utils.zip", 
+    "155178106", "0x8D87F67F4AEB960"),
+    ("https://mind201910small.blob.core.windows.net/release/MINDlarge_train.zip",
+    "530196631","0x8D8244E90C15C07"),
+    ("https://mind201910small.blob.core.windows.net/release/MINDlarge_dev.zip",
+    "103456245","0x8D8244E92005849"),
+    ("https://mind201910small.blob.core.windows.net/release/MINDlarge_utils.zip", 
+    "150359301", "0x8D87F67E6CA4364"),
+    ])
+def test_mind_url(url, content_length, etag):
+    """ Test file sizes and etags. 
+    Covers train, dev and utils files for demo, small and large datasets. 
+    """
+    url_headers = requests.head(url).headers
+    assert url_headers["Content-Length"] == content_length
+    assert url_headers["ETag"] == etag
 
-
-
-
-
-# @pytest.mark.smoke
-# def test_download_mind(tmp):
-#     train_path, valid_path = download_mind(size="small", dest_path=tmp)
-#     statinfo = os.stat(train_path)
-#     assert statinfo.st_size == 52952752
-#     statinfo = os.stat(valid_path)
-#     assert statinfo.st_size == 30945572
-
-
-URL_INFO = {
-    "large": {
-        "train": {
-            "url": "https://mind201910small.blob.core.windows.net/release/MINDlarge_train.zip",
-            "headers": {
-                'Content-Length': '530196631',
-                'ETag': '0x8D8244E90C15C07'
-            }
-        },
-        "valid": {
-            "url": "https://mind201910small.blob.core.windows.net/release/MINDlarge_dev.zip",
-            "headers": {
-                'Content-Length': '103456245',
-                'ETag': '0x8D8244E92005849'
-            }
-        },
-    },
-    "small": {
-        "train": {
-            "url": "https://mind201910small.blob.core.windows.net/release/MINDsmall_train.zip",
-            "headers": {
-                'Content-Length': '52952752',
-                'ETag': '0x8D834F2EB31BDEC'
-            }
-        },
-        "valid": {
-            "url": "https://mind201910small.blob.core.windows.net/release/MINDsmall_dev.zip",
-            "headers": {
-                'Content-Length': '30945572',
-                'ETag': '0x8D834F2EBA8D865'
-            }
-        }
-    },
-    "demo": {
-        "train": {
-            "url": "https://recodatasets.blob.core.windows.net/newsrec/MINDdemo_train.zip",
-            "headers": {
-                'Content-Length': '17372879',
-                'ETag': '0x8D82C63E386D09C'
-            }
-        },
-        "valid": {
-            "url": "https://recodatasets.blob.core.windows.net/newsrec/MINDdemo_dev.zip",
-            "headers": {
-                'Content-Length': '10080022',
-                'ETag': '0x8D82C6434EC3CEE'
-            }
-        },
-        "utils": {
-            "url": "https://recodatasets.blob.core.windows.net/newsrec/MINDdemo_utils.zip",
-            "headers": {
-                'Content-Length': '97292490',
-                'ETag': '0x8D82C66AF165260'
-            }
-        }
-    }
-}
-
-@pytest.mark.smoke
-def test_mind_urls():
-    """ Test headers (without downloading) of all 7 URLs for demo, small and large for file size and ETag """
-    for mind_size in URL_INFO:
-        for dataset_type in URL_INFO[mind_size]:
-            url_headers = requests.head(URL_INFO[mind_size][dataset_type]["url"]).headers
-            for hdr in URL_INFO[mind_size][dataset_type]["headers"]:
-                assert url_headers[hdr] == URL_INFO[mind_size][dataset_type]["headers"][hdr]
-
-
-
-FILESIZES = {
-    "demo":{
-        "train":{
-            "behaviors.tsv": 14707247,
-            "entity_embedding.vec": 16077470,
-            "news.tsv": 23120370,
-            "relation_embedding.vec": 1044588
-        },
-        "valid":{
-            "behaviors.tsv": 4434762,
-            "entity_embedding.vec": 11591565,
-            "news.tsv": 15624320,
-            "relation_embedding.vec": 1044588
-        }
-    },
-    "small":{
-        "train":{
-            "behaviors.tsv": 92019716,
-            "entity_embedding.vec": 25811015,
-            "news.tsv": 41202121,
-            "relation_embedding.vec": 1044588
-        },
-        "valid":{
-            "behaviors.tsv": 42838544,
-            "entity_embedding.vec": 21960998,
-            "news.tsv": 33519092,
-            "relation_embedding.vec": 1044588
-        }
-    }
-}
-
-@pytest.mark.smoke
-def test_extract_mind(tmp):
-    """ Only extract demo and small train and valid files """
-    for dataset_size in FILESIZES: #demo or small
-        train_zip, valid_zip = download_mind(size=dataset_size, dest_path=tmp)
-        unzipped_paths = extract_mind(train_zip, valid_zip)
-        assert len(unzipped_paths) == 2
-        for pos in range(len(unzipped_paths)): # either train_path or valid_path
-            for filename in FILESIZES[dataset_size][train_or_valid[pos]]:
-                statinfo = os.stat(os.path.join(unzipped_paths[pos], filename))
-                assert statinfo.st_size == FILESIZES[dataset_size][train_or_valid[pos]][filename] 
-    
