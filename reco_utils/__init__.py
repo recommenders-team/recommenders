312d3902b34b64a864c67f3b5ce801eb109a4b99
Daniel Ciborowski <dciborow@microsoft.com>
2021-02-25 01:03:34 -0500

    Update __init__.py

--- reco_utils/__init__.py
+++ reco_utils/__init__.py
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 
 __title__ = "Microsoft Recommenders"
-__version__ = "2021.2.2"
+__version__ = "2021.2.3"
 __author__ = "RecoDev Team at Microsoft"
 __license__ = "MIT"
 __copyright__ = "Copyright 2018-present Microsoft Corporation"
