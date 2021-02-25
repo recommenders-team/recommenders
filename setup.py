7c692c202cda7878011ad2baeda91ff20d4a3dd2
Daniel Ciborowski <dciborow@microsoft.com>
2021-02-25 01:27:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,51 @@ HASH = environ.get("HASH", None)
 if HASH is not None:
     version += ".post" + str(int(time.time()))
 
+DEPENDENCIES = [
+    'flask==1.1.1',
+    'requests==2.24.0',
+    'pandas==1.1.3',
+	'azureml-sdk[notebooks,tensorboard]==1.22.0',
+	'azure-storage-blob<12.8.0',
+	'azure-cli-core==2.19.1',
+	'azure-mgmt-cosmosdb==6.0.0',
+	'black>=18.6b4',
+	'category_encoders>=1.3.0',
+	'dataclasses>=0.6',
+	'hyperopt==0.2.5',
+	'idna==3.1',
+	'locustio==0.999',
+	'memory-profiler>=0.54.0',
+	'nbconvert==6.0.7',
+	'pydocumentdb>=2.3.3',
+	'pymanopt==0.2.5',
+	'xlearn==0.40a1',
+	'tensorflow==2.4.1',
+	'transformers==4.3.2',
+	'tensorflow==2.4.0',
+	'scrapbook>=0.5.0',
+    'bottleneck==1.2.1',
+    'dask>=0.17.1',
+    'fastparquet>=0.1.6',
+    'ipykernel>=4.6.1',
+    'jupyter>=1.0.0',
+    'lightfm==1.15',
+    'matplotlib>=2.2.2',
+    'mock==2.0.0',
+    'nltk>=3.4',
+    'numpy>=1.13.3',
+	'pytorch-cpu>=1.0.0',
+    'seaborn>=0.8.1',
+    'scikit-learn>=0.19.1',
+    'scipy>=1.0.0',
+    'scikit-surprise>=1.0.6',
+    'swig==3.0.12',
+    'lightgbm==2.2.1',
+    'cornac>=1.1.2',
+    'papermill>=2.2.0',
+    'tqdm>=4.31.1',
+]
+
 name = environ.get("LIBRARY_NAME", "pre_reco_utils")
 
 setup(
@@ -39,5 +84,6 @@ setup(
     keywords="recommendations recommenders recommender system engine machine learning python spark gpu",
     package_dir={"reco_utils": "reco_utils"},
     packages=find_packages(where=".", exclude=["tests", "tools", "examples"]),
+    install_requires=DEPENDENCIES,
     python_requires=">=3.6, <4",
 )
