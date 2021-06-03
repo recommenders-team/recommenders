31d3a4f078abceae328266c681540b404401589f
miguelgfierro <miguelgfierro@users.noreply.github.com>
2021-06-03 14:35:11 +0000

    remove xlearn from core deps :bug:

--- tests/unit/reco_utils/dataset/test_dataset.py
+++ tests/unit/reco_utils/dataset/test_dataset.py
@@ -2,7 +2,6 @@
 # Licensed under the MIT License.
 
 import os
-import sys
 import pytest
 from tempfile import TemporaryDirectory
 from reco_utils.dataset.download_utils import maybe_download, download_path
@@ -12,8 +11,10 @@ def test_maybe_download():
     file_url = "https://raw.githubusercontent.com/Microsoft/Recommenders/main/LICENSE"
     filepath = "license.txt"
     assert not os.path.exists(filepath)
+
     filepath = maybe_download(file_url, "license.txt", expected_bytes=1162)
     assert os.path.exists(filepath)
+
     os.remove(filepath)
     with pytest.raises(IOError):
         filepath = maybe_download(file_url, "license.txt", expected_bytes=0)
