2ccae9a29cbdb8a58debbf5d44699b258e84899e
YanZhangADS <YanZhangADS@users.noreply.github.com>
2021-06-06 11:23:04 +0000

    fix

--- tests/unit/reco_utils/evaluation/test_spark_diversity_evaluator.py
+++ tests/unit/reco_utils/evaluation/test_spark_diversity_evaluator.py
@@ -1,9 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import itertools
 import pytest
-import numpy as np
 import pandas as pd
 from pandas.util.testing import assert_frame_equal
 
