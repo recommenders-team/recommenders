34c208c1175e8f0c3592c0eb0564a6f21d5903e0
miguelgfierro <miguelgfierro@users.noreply.github.com>
2021-07-16 13:28:44 +0000

    update skip test list

--- tests/unit/examples/test_notebooks_python.py
+++ tests/unit/examples/test_notebooks_python.py
@@ -56,7 +56,7 @@ def test_surprise_deep_dive_runs(notebooks, output_notebook, kernel_name):
 
 
 @pytest.mark.notebooks
-@pytest.mark.skip(reason="Tests removed due to installation incompatibilities")
+@pytest.mark.skip(reason="VW pip package has installation incompatibilities")
 def test_vw_deep_dive_runs(notebooks, output_notebook, kernel_name):
     notebook_path = notebooks["vowpal_wabbit_deep_dive"]
     pm.execute_notebook(notebook_path, output_notebook, kernel_name=kernel_name)
