96d94b0ebcd92e0a7bc73a53a9da9ba310aaca75
Abir <aeroabir@users.noreply.github.com>
2021-10-11 16:45:20 +0000

    tensorflow-2.6

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+from os import environ
 from pathlib import Path
 from setuptools import setup, find_packages
+import site
+import sys
 import time
-from os import environ
 
-# Version
+# workround for enabling editable user pip installs
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+# version
 here = Path(__file__).absolute().parent
 version_data = {}
 with open(here.joinpath("recommenders", "__init__.py"), "r") as f:
@@ -22,15 +27,15 @@ if HASH is not None:
     version += ".post" + str(int(time.time()))
 
 install_requires = [
-    "numpy<1.19.0,>=1.16.0",
+    "numpy>=1.19",  # 1.19 required by tensorflow
     "pandas>1.0.3,<2",
-    "scipy==1.4.1",
+    "scipy>=1.0.0,<2",
     "tqdm>=4.31.1,<5",
     "matplotlib>=2.2.2,<4",
     "scikit-learn>=0.22.1,<1",
-    # "numba>=0.38.1,<1",
+    "numba>=0.38.1,<1",
     "lightfm>=1.15,<2",
-    "lightgbm>=2.2.1,<3",
+    "lightgbm>=2.2.1",
     "memory_profiler>=0.54.0,<1",
     "nltk>=3.4,<4",
     "pydocumentdb>=2.3.3<3",  # TODO: replace with azure-cosmos
@@ -43,7 +48,8 @@ install_requires = [
     "pyyaml>=5.4.1,<6",
     "requests>=2.0.0,<3",
     "cornac>=1.1.2,<2",
-    "scikit-surprise>=0.19.1,<=1.1.1",
+    # For Surprise, specify the tarball in order to avoid incompatibilities of compiled .pyx files with numpy versions < 1.20
+    "scikit-surprise@https://files.pythonhosted.org/packages/97/37/5d334adaf5ddd65da99fc65f6507e0e4599d092ba048f4302fe8775619e8/scikit-surprise-1.1.1.tar.gz",
     "retrying>=1.3.3",
 ]
 
@@ -52,7 +58,7 @@ extras_require = {
     "examples": [
         "azure.mgmt.cosmosdb>=0.8.0,<1",
         "hyperopt>=0.1.2,<1",
-        "ipykernel>=4.6.1,<5",
+        "ipykernel>=4.6.1,<7",
         "jupyter>=1,<2",
         "locust>=1,<2",
         "papermill>=2.1.2,<3",
@@ -60,10 +66,8 @@ extras_require = {
     ],
     "gpu": [
         "nvidia-ml-py3>=7.352.0",
-        "tensorflow-gpu==2.3.0",  # compiled with CUDA 10.0
-        "torch==1.9.1",  # last os-common version with CUDA 10.0 support
-        "six~=1.15.0",
-        "typing-extensions~=3.7.4",
+        "tensorflow>=2.6",  # compiled with CUDA 11.2, cudnn 8.1
+        "torch==1.2.0",  # last os-common version with CUDA 10.0 support
         "fastai>=1.0.46,<2",
     ],
     "spark": [
@@ -75,7 +79,13 @@ extras_require = {
         "cmake>=3.18.4.post1",
         "xlearn==0.40a1",
     ],
-    "dev": ["black>=18.6b4,<21", "pytest>=3.6.4", "pytest-cov>=2.12.1"],
+    "dev": [
+        "black>=18.6b4,<21",
+        "pandera[strategies]>=0.6.5",  # For generating fake datasets
+        "pytest>=3.6.4",
+        "pytest-cov>=2.12.1",
+        "pytest-mock>=3.6.1",  # for access to mock fixtures in pytest
+    ],
 }
 # for the brave of heart
 extras_require["all"] = list(set(sum([*extras_require.values()], [])))
@@ -119,6 +129,9 @@ setup(
     "machine learning python spark gpu",
     install_requires=install_requires,
     package_dir={"recommenders": "recommenders"},
-    packages=find_packages(where=".", exclude=["tests", "tools", "examples"]),
+    packages=find_packages(
+        where=".",
+        exclude=["contrib", "docs", "examples", "scenarios", "tests", "tools"],
+    ),
     python_requires=">=3.6, <3.8",
 )
