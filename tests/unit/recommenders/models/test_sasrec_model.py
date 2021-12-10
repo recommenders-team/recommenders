55ad6fc502fe6545c44c9a9e4da1520c22ddd71c
Abir <aeroabir@users.noreply.github.com>
2021-12-10 14:12:39 +0000

    fix data download

--- tests/unit/recommenders/models/test_sasrec_model.py
+++ tests/unit/recommenders/models/test_sasrec_model.py
@@ -3,21 +3,120 @@
 
 import pytest
 import os
+from collections import defaultdict
 
-from recommenders.models.sasrec.model import SASREC
-from recommenders.models.sasrec.ssept import SSEPT
-from recommenders.models.sasrec.sampler import WarpSampler
-from recommenders.models.sasrec.util import SASRecDataSet
+try:
+    from recommenders.models.sasrec.model import SASREC
+    from recommenders.models.sasrec.ssept import SSEPT
+    from recommenders.models.sasrec.sampler import WarpSampler
+    from recommenders.models.sasrec.util import SASRecDataSet
+
+    from recommenders.datasets.amazon_reviews import (
+        download_and_extract,
+        _reviews_preprocessing,
+    )
+
+except ImportError:
+    pass  # skip if in cpu environment
+
+
+def data_process_with_time(fname, pname, K=10, sep=" ", item_set=None, add_time=False):
+    User = defaultdict(list)
+    Users = set()
+    Items = set()
+    user_dict, item_dict = {}, {}
+
+    item_counter = defaultdict(lambda: 0)
+    user_counter = defaultdict(lambda: 0)
+    with open(fname, "r") as fr:
+        for line in fr:
+            u, i, t = line.rstrip().split(sep)
+            User[u].append((i, t))
+            Items.add(i)
+            Users.add(u)
+            item_counter[i] += 1
+            user_counter[u] += 1
+
+    # remove items with less than K interactions
+    print(f"Read {len(User)} users and {len(Items)} items")
+    remove_items = set()
+    count_remove, count_missing = 0, 0
+    for item in Items:
+        if item_counter[item] < K:
+            count_remove += 1
+            remove_items.add(item)
+        elif item_set and item not in item_set:
+            count_missing += 1
+            remove_items.add(item)
+
+    if count_remove > 0:
+        print(f"{count_remove} items have less than {K} interactions")
+
+    if count_missing > 0:
+        print(f"{count_missing} items are not in the meta data")
+
+    Items = Items - remove_items
+
+    # remove users with less than K interactions
+    remove_users = set()
+    count_remove = 0
+    # Users = set(User.keys())
+    for user in Users:
+        if user_counter[user] < K:
+            remove_users.add(user)
+            count_remove += 1
+    if count_remove > 0:
+        print(f"{count_remove} users have less than {K} interactions")
+        Users = Users - remove_users
+
+    print(f"Total {len(Users)} users and {len(Items)} items")
+    item_count = 1
+    for item in Items:
+        item_dict[item] = item_count
+        item_count += 1
+
+    count_del = 0
+    user_count = 1
+    with open(pname, "w") as fw:
+        for user in Users:
+            items = User[user]
+            items = [tup for tup in items if tup[0] in Items]
+            if len(items) < K:
+                count_del += 1
+            else:
+                user_dict[user] = user_count
+                # sort by time
+                items = sorted(items, key=lambda x: x[1])
+
+                # replace by the item-code
+                timestamps = [x[1] for x in items]
+                items = [item_dict[x[0]] for x in items]
+                for i, t in zip(items, timestamps):
+                    out_txt = [str(user_count), str(i)]
+                    if add_time:
+                        out_txt.append(str(t))
+                    fw.write(sep.join(out_txt) + "\n")
+                user_count += 1
+
+    print(f"Total {user_count-1} users, {count_del} removed")
+    print(f"Processed model input data in {pname}")
+    return user_dict, item_dict
 
 
 @pytest.mark.gpu
 def test_prepare_data():
-    data_dir = "/recsys_data/RecSys/SASRec-tf2/data/"
-    dataset = "ae"
-    inp_file = os.path.join(data_dir, dataset + ".txt")
+    data_dir = os.path.join("..", "..", "tests", "resources", "deeprec", "sasrec")
+    dataset = "reviews_Electronics_5"
+    reviews_name = dataset + ".json"
+    outfile = os.path.join(data_dir, dataset + ".txt")
+
+    reviews_file = os.path.join(data_dir, reviews_name)
+    download_and_extract(reviews_name, reviews_file)
+    reviews_output = _reviews_preprocessing(reviews_file)
+    _, _ = data_process_with_time(reviews_output, outfile, K=10, sep="\t")
 
     # initiate a dataset class
-    data = SASRecDataSet(filename=inp_file)
+    data = SASRecDataSet(filename=outfile, col_sep="\t")
 
     # create train, validation and test splits
     data.split()
@@ -29,14 +128,20 @@ def test_prepare_data():
 
 @pytest.mark.gpu
 def test_sampler():
-    data_dir = "/recsys_data/RecSys/SASRec-tf2/data/"
-    dataset = "ae"
     batch_size = 8
     maxlen = 50
-    inp_file = os.path.join(data_dir, dataset + ".txt")
+    data_dir = os.path.join("..", "..", "tests", "resources", "deeprec", "sasrec")
+    dataset = "reviews_Electronics_5"
+    reviews_name = dataset + ".json"
+    outfile = os.path.join(data_dir, dataset + ".txt")
+
+    reviews_file = os.path.join(data_dir, reviews_name)
+    download_and_extract(reviews_name, reviews_file)
+    reviews_output = _reviews_preprocessing(reviews_file)
+    _, _ = data_process_with_time(reviews_output, outfile, K=10, sep="\t")
 
     # initiate a dataset class
-    data = SASRecDataSet(filename=inp_file)
+    data = SASRecDataSet(filename=outfile, col_sep="\t")
 
     # create train, validation and test splits
     data.split()
