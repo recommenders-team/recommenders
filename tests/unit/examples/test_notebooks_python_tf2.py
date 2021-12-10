0a560dbbf246badaa96070c680f0578b38ad97c0
Abir <aeroabir@users.noreply.github.com>
2021-12-10 15:00:45 +0000

    mark gpu

--- tests/unit/examples/test_notebooks_python_tf2.py
+++ tests/unit/examples/test_notebooks_python_tf2.py
@@ -16,6 +16,7 @@ ABS_TOL = 0.05
 
 
 @pytest.mark.notebooks
+@pytest.mark.gpu
 def test_sasrec_single_node_runs(notebooks, output_notebook, kernel_name):
     notebook_path = notebooks["sasrec_quickstart"]
     pm.execute_notebook(notebook_path, output_notebook, kernel_name=kernel_name)
