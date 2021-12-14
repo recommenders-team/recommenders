4ec139a3bde4149974c239221e9647124ac3a506
Simon Zhao <43029286+simonzhaoms@users.noreply.github.com>
2021-12-14 16:18:07 +0800

    Corrent VERSION path

--- contrib/sarplus/scala/python/setup.py
+++ contrib/sarplus/scala/python/setup.py
@@ -6,7 +6,7 @@ import os
 
 setup(
     name="pysarplus_dummy",
-    version=(Path(__file__).resolve().parent / "VERSION").read_text().strip(),
+    version=(Path(__file__).resolve().parent.parent.parent / "VERSION").read_text().strip(),
     description="pysarplus dummy package to trigger spark packaging",
     author="RecoDev Team at Microsoft",
     author_email="recodevteam@service.microsoft.com",
