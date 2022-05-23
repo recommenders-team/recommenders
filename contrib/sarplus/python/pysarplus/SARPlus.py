870cbe7366cfab26cf4445def4d1826333acb132
Simon Zhao <43029286+simonzhaoms@users.noreply.github.com>
2022-05-23 15:15:33 +0800

    Typos and fix static analysis warnings

--- contrib/sarplus/python/pysarplus/SARPlus.py
+++ contrib/sarplus/python/pysarplus/SARPlus.py
@@ -271,7 +271,7 @@ class SARPlus:
           SELECT a.{col_user}, a.{col_item}, CAST(a.{col_rating} AS double) {col_rating}
           FROM {prefix}df_train a INNER JOIN {prefix}df_test_users b ON a.{col_user} = b.{col_user} 
           DISTRIBUTE BY {col_user}
-          SORT BY {col_user}, {col_item}          
+          SORT BY {col_user}, {col_item}
         """
         )
 
