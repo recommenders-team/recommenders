b49013715ca3e6ead54862172fcb2b07412c906c
miguelgfierro <miguelgfierro@users.noreply.github.com>
2022-11-11 12:58:49 +0000

    test gpu benchmark

--- tests/integration/examples/test_notebooks_gpu.py
+++ tests/integration/examples/test_notebooks_gpu.py
@@ -733,7 +733,7 @@ def test_sasrec_quickstart_integration(
     [
         (
             ["100k"],
-            ["lightgcn"], #["ncf", "fastai", "bivae", "lightgcn"],
+            ["ncf", "fastai", "bivae", "lightgcn"],
             dict(
                 eval_precision=0.131601,
                 eval_recall=0.038056,
