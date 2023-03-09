168a1723197b8a6241682cdf8e7b9b688f1cf151
miguelgfierro <miguelgfierro@users.noreply.github.com>
2023-03-09 17:16:19 +0100

    add env PYSPARK

--- tests/ci/azureml_tests/test_groups.py
+++ tests/ci/azureml_tests/test_groups.py
@@ -131,15 +131,15 @@ nightly_test_groups = {
         # "tests/integration/examples/test_notebooks_gpu.py::test_sasrec_quickstart_integration",  # 448.06s + 614.69s
     ],
     "group_spark_001": [  # Total group time: 987.16s
-        "tests/smoke/recommenders/dataset/test_movielens.py::test_load_spark_df",  # 4.33s
-        "tests/integration/recommenders/datasets/test_movielens.py::test_load_spark_df",  # 25.58s + 101.99s + 139.23s
-        #
-        "tests/smoke/recommenders/dataset/test_criteo.py::test_criteo_load_spark_df",  # 6.83s
-        "tests/smoke/examples/test_notebooks_pyspark.py::test_mmlspark_lightgbm_criteo_smoke",  # 32.45s
-        "tests/integration/recommenders/datasets/test_criteo.py::test_criteo_load_spark_df",  # 374.64s
-        #
-        "tests/smoke/examples/test_notebooks_pyspark.py::test_als_pyspark_smoke",  # 49.53s
-        "tests/integration/examples/test_notebooks_pyspark.py::test_als_pyspark_integration",  # 110.58s
+        # "tests/smoke/recommenders/dataset/test_movielens.py::test_load_spark_df",  # 4.33s
+        # "tests/integration/recommenders/datasets/test_movielens.py::test_load_spark_df",  # 25.58s + 101.99s + 139.23s
+        # #
+        # "tests/smoke/recommenders/dataset/test_criteo.py::test_criteo_load_spark_df",  # 6.83s
+        # "tests/smoke/examples/test_notebooks_pyspark.py::test_mmlspark_lightgbm_criteo_smoke",  # 32.45s
+        # "tests/integration/recommenders/datasets/test_criteo.py::test_criteo_load_spark_df",  # 374.64s
+        # #
+        # "tests/smoke/examples/test_notebooks_pyspark.py::test_als_pyspark_smoke",  # 49.53s
+        # "tests/integration/examples/test_notebooks_pyspark.py::test_als_pyspark_integration",  # 110.58s
         "tests/integration/examples/test_notebooks_pyspark.py::test_benchmark_movielens_pyspark",  # 142
     ],
 }
@@ -182,97 +182,97 @@ unit_test_groups = {
         "tests/unit/recommenders/evaluation/test_spark_evaluation.py::test_distributional_coverage",
         "tests/unit/recommenders/datasets/test_spark_splitter.py::test_min_rating_filter",
     ],
-    "group_notebooks_spark_001": [  # Total group time: 794s
-        "tests/unit/examples/test_notebooks_pyspark.py::test_als_deep_dive_runs",  # 287.70s
-        "tests/unit/examples/test_notebooks_pyspark.py::test_als_pyspark_runs",  # 374.15s
-        "tests/unit/examples/test_notebooks_pyspark.py::test_mmlspark_lightgbm_criteo_runs",  # 132.09s
-    ],
-    "group_notebooks_spark_002": [  # Total group time: 466s
-        "tests/unit/examples/test_notebooks_pyspark.py::test_data_split_runs",  # 43.66s
-        "tests/unit/examples/test_notebooks_pyspark.py::test_evaluation_runs",  # 45.24s
-        "tests/unit/examples/test_notebooks_pyspark.py::test_evaluation_diversity_runs",  # 376.61s
-        # TODO: This is a flaky test, skip for now, to be fixed in future iterations.
-        # Refer to the issue: https://github.com/microsoft/recommenders/issues/1770
-        # "tests/unit/examples/test_notebooks_pyspark.py::test_spark_tuning",  # 212.29s+190.02s+180.13s+164.09s=746.53s (flaky test, it rerun several times)
-    ],
-    "group_gpu_001": [  # Total group time: 492.62s
-        "tests/unit/examples/test_notebooks_gpu.py::test_gpu_vm",  # 0.76s (Always the first test to check the GPU works)
-        "tests/unit/recommenders/models/test_deeprec_model.py::test_xdeepfm_component_definition",
-        "tests/unit/recommenders/models/test_deeprec_model.py::test_dkn_component_definition",
-        "tests/unit/recommenders/models/test_deeprec_model.py::test_dkn_item2item_component_definition",
-        "tests/unit/recommenders/models/test_deeprec_model.py::test_slirec_component_definition",
-        "tests/unit/recommenders/models/test_deeprec_model.py::test_nextitnet_component_definition",
-        "tests/unit/recommenders/models/test_deeprec_model.py::test_sum_component_definition",
-        "tests/unit/recommenders/models/test_deeprec_model.py::test_lightgcn_component_definition",
-        "tests/unit/recommenders/models/test_rbm.py::test_sampling_funct",
-        "tests/unit/recommenders/models/test_rbm.py::test_train_param_init",
-        "tests/unit/recommenders/models/test_rbm.py::test_save_load",
-        "tests/unit/recommenders/models/test_wide_deep_utils.py::test_wide_model",
-        "tests/unit/recommenders/models/test_ncf_singlenode.py::test_neumf_save_load",
-        "tests/unit/recommenders/models/test_ncf_singlenode.py::test_regular_save_load",
-        "tests/unit/recommenders/utils/test_tf_utils.py::test_evaluation_log_hook",
-        "tests/unit/recommenders/utils/test_tf_utils.py::test_pandas_input_fn_for_saved_model",
-        "tests/unit/recommenders/models/test_wide_deep_utils.py::test_wide_deep_model",
-        "tests/unit/recommenders/models/test_newsrec_model.py::test_naml_component_definition",
-        "tests/unit/recommenders/models/test_newsrec_model.py::test_lstur_component_definition",
-        "tests/unit/recommenders/models/test_newsrec_model.py::test_nrms_component_definition",
-        "tests/unit/recommenders/models/test_wide_deep_utils.py::test_deep_model",
-        "tests/unit/recommenders/models/test_newsrec_model.py::test_npa_component_definition",
-        "tests/unit/recommenders/models/test_ncf_singlenode.py::test_fit",
-        "tests/unit/recommenders/models/test_ncf_singlenode.py::test_init",
-        "tests/unit/recommenders/models/test_ncf_dataset.py::test_test_loader",
-        "tests/unit/recommenders/models/test_ncf_dataset.py::test_datafile_init",
-        "tests/unit/recommenders/models/test_ncf_dataset.py::test_train_loader",
-        "tests/unit/recommenders/models/test_rbm.py::test_class_init",
-        "tests/unit/recommenders/utils/test_tf_utils.py::test_pandas_input_fn",
-        "tests/unit/recommenders/models/test_ncf_dataset.py::test_datafile_init_unsorted",
-        "tests/unit/recommenders/models/test_ncf_singlenode.py::test_predict",
-        "tests/unit/recommenders/models/test_ncf_dataset.py::test_datafile_missing_column",
-        "tests/unit/recommenders/models/test_sasrec_model.py::test_prepare_data",
-        "tests/unit/recommenders/models/test_sasrec_model.py::test_sampler",
-        "tests/unit/recommenders/models/test_sasrec_model.py::test_sasrec",
-        "tests/unit/recommenders/models/test_sasrec_model.py::test_ssept",
-    ],
-    "group_notebooks_gpu_001": [  # Total group time: 563.35s
-        "tests/unit/examples/test_notebooks_gpu.py::test_gpu_vm",  # 0.76s (Always the first test to check the GPU works)
-        "tests/unit/examples/test_notebooks_gpu.py::test_dkn_quickstart",
-        "tests/unit/examples/test_notebooks_gpu.py::test_ncf",
-        "tests/unit/examples/test_notebooks_gpu.py::test_ncf_deep_dive",
-        "tests/unit/examples/test_notebooks_gpu.py::test_fastai",
-    ],
-    "group_notebooks_gpu_002": [  # Total group time: 241.15s
-        "tests/unit/examples/test_notebooks_gpu.py::test_gpu_vm",  # 0.76s (Always the first test to check the GPU works)
-        "tests/unit/examples/test_notebooks_gpu.py::test_wide_deep",
-        "tests/unit/examples/test_notebooks_gpu.py::test_xdeepfm",
-        "tests/unit/examples/test_notebooks_gpu.py::test_gpu_vm",
-    ],
-    "group_cpu_001": [  # Total group time: 525.96s
-        "tests/unit/recommenders/datasets/test_movielens.py::test_load_pandas_df_mock_100__with_default_param__succeed",
-        "tests/unit/recommenders/datasets/test_dataset.py::test_maybe_download_wrong_bytes",
-        "tests/unit/recommenders/datasets/test_movielens.py::test_mock_movielens_schema__has_default_col_names",
-        "tests/unit/recommenders/datasets/test_movielens.py::test_load_pandas_df_mock_100__with_custom_param__succeed",
-        "tests/unit/recommenders/datasets/test_dataset.py::test_maybe_download_retry",
-        "tests/unit/recommenders/datasets/test_movielens.py::test_mock_movielens_schema__get_df__return_success",
-        "tests/unit/recommenders/utils/test_timer.py::test_timer",
-        "tests/unit/recommenders/tuning/test_ncf_utils.py::test_compute_test_results__return_success",
-        "tests/unit/recommenders/datasets/test_movielens.py::test_mock_movielens_schema__get_df_remove_default_col__return_success",
-        "tests/unit/recommenders/models/test_geoimc.py::test_imcproblem",
-        "tests/unit/recommenders/datasets/test_wikidata.py::test_find_wikidata_id",
-        "tests/unit/recommenders/models/test_sar_singlenode.py::test_sar_item_similarity",
-        "tests/unit/recommenders/models/test_tfidf_utils.py::test_tokenize_text",
-        "tests/unit/recommenders/models/test_tfidf_utils.py::test_get_tokens",
-        "tests/unit/recommenders/models/test_cornac_utils.py::test_recommend_k_items",
-        "tests/unit/recommenders/evaluation/test_python_evaluation_time_performance.py",  # 297.91s
-    ],
-    "group_notebooks_cpu_001": [  # Total group time: 226.42s
-        "tests/unit/examples/test_notebooks_python.py::test_rlrmc_quickstart_runs",
-        "tests/unit/examples/test_notebooks_python.py::test_sar_deep_dive_runs",
-        "tests/unit/examples/test_notebooks_python.py::test_baseline_deep_dive_runs",
-        "tests/unit/examples/test_notebooks_python.py::test_template_runs",
-        "tests/unit/recommenders/utils/test_notebook_utils.py::test_is_jupyter",
-        "tests/unit/examples/test_notebooks_python.py::test_surprise_deep_dive_runs",
-        "tests/unit/examples/test_notebooks_python.py::test_lightgbm",
-        "tests/unit/examples/test_notebooks_python.py::test_cornac_deep_dive_runs",
-        "tests/unit/examples/test_notebooks_python.py::test_sar_single_node_runs",
-    ],
+    # "group_notebooks_spark_001": [  # Total group time: 794s
+    #     "tests/unit/examples/test_notebooks_pyspark.py::test_als_deep_dive_runs",  # 287.70s
+    #     "tests/unit/examples/test_notebooks_pyspark.py::test_als_pyspark_runs",  # 374.15s
+    #     "tests/unit/examples/test_notebooks_pyspark.py::test_mmlspark_lightgbm_criteo_runs",  # 132.09s
+    # ],
+    # "group_notebooks_spark_002": [  # Total group time: 466s
+    #     "tests/unit/examples/test_notebooks_pyspark.py::test_data_split_runs",  # 43.66s
+    #     "tests/unit/examples/test_notebooks_pyspark.py::test_evaluation_runs",  # 45.24s
+    #     "tests/unit/examples/test_notebooks_pyspark.py::test_evaluation_diversity_runs",  # 376.61s
+    #     # TODO: This is a flaky test, skip for now, to be fixed in future iterations.
+    #     # Refer to the issue: https://github.com/microsoft/recommenders/issues/1770
+    #     # "tests/unit/examples/test_notebooks_pyspark.py::test_spark_tuning",  # 212.29s+190.02s+180.13s+164.09s=746.53s (flaky test, it rerun several times)
+    # ],
+    # "group_gpu_001": [  # Total group time: 492.62s
+    #     "tests/unit/examples/test_notebooks_gpu.py::test_gpu_vm",  # 0.76s (Always the first test to check the GPU works)
+    #     "tests/unit/recommenders/models/test_deeprec_model.py::test_xdeepfm_component_definition",
+    #     "tests/unit/recommenders/models/test_deeprec_model.py::test_dkn_component_definition",
+    #     "tests/unit/recommenders/models/test_deeprec_model.py::test_dkn_item2item_component_definition",
+    #     "tests/unit/recommenders/models/test_deeprec_model.py::test_slirec_component_definition",
+    #     "tests/unit/recommenders/models/test_deeprec_model.py::test_nextitnet_component_definition",
+    #     "tests/unit/recommenders/models/test_deeprec_model.py::test_sum_component_definition",
+    #     "tests/unit/recommenders/models/test_deeprec_model.py::test_lightgcn_component_definition",
+    #     "tests/unit/recommenders/models/test_rbm.py::test_sampling_funct",
+    #     "tests/unit/recommenders/models/test_rbm.py::test_train_param_init",
+    #     "tests/unit/recommenders/models/test_rbm.py::test_save_load",
+    #     "tests/unit/recommenders/models/test_wide_deep_utils.py::test_wide_model",
+    #     "tests/unit/recommenders/models/test_ncf_singlenode.py::test_neumf_save_load",
+    #     "tests/unit/recommenders/models/test_ncf_singlenode.py::test_regular_save_load",
+    #     "tests/unit/recommenders/utils/test_tf_utils.py::test_evaluation_log_hook",
+    #     "tests/unit/recommenders/utils/test_tf_utils.py::test_pandas_input_fn_for_saved_model",
+    #     "tests/unit/recommenders/models/test_wide_deep_utils.py::test_wide_deep_model",
+    #     "tests/unit/recommenders/models/test_newsrec_model.py::test_naml_component_definition",
+    #     "tests/unit/recommenders/models/test_newsrec_model.py::test_lstur_component_definition",
+    #     "tests/unit/recommenders/models/test_newsrec_model.py::test_nrms_component_definition",
+    #     "tests/unit/recommenders/models/test_wide_deep_utils.py::test_deep_model",
+    #     "tests/unit/recommenders/models/test_newsrec_model.py::test_npa_component_definition",
+    #     "tests/unit/recommenders/models/test_ncf_singlenode.py::test_fit",
+    #     "tests/unit/recommenders/models/test_ncf_singlenode.py::test_init",
+    #     "tests/unit/recommenders/models/test_ncf_dataset.py::test_test_loader",
+    #     "tests/unit/recommenders/models/test_ncf_dataset.py::test_datafile_init",
+    #     "tests/unit/recommenders/models/test_ncf_dataset.py::test_train_loader",
+    #     "tests/unit/recommenders/models/test_rbm.py::test_class_init",
+    #     "tests/unit/recommenders/utils/test_tf_utils.py::test_pandas_input_fn",
+    #     "tests/unit/recommenders/models/test_ncf_dataset.py::test_datafile_init_unsorted",
+    #     "tests/unit/recommenders/models/test_ncf_singlenode.py::test_predict",
+    #     "tests/unit/recommenders/models/test_ncf_dataset.py::test_datafile_missing_column",
+    #     "tests/unit/recommenders/models/test_sasrec_model.py::test_prepare_data",
+    #     "tests/unit/recommenders/models/test_sasrec_model.py::test_sampler",
+    #     "tests/unit/recommenders/models/test_sasrec_model.py::test_sasrec",
+    #     "tests/unit/recommenders/models/test_sasrec_model.py::test_ssept",
+    # ],
+    # "group_notebooks_gpu_001": [  # Total group time: 563.35s
+    #     "tests/unit/examples/test_notebooks_gpu.py::test_gpu_vm",  # 0.76s (Always the first test to check the GPU works)
+    #     "tests/unit/examples/test_notebooks_gpu.py::test_dkn_quickstart",
+    #     "tests/unit/examples/test_notebooks_gpu.py::test_ncf",
+    #     "tests/unit/examples/test_notebooks_gpu.py::test_ncf_deep_dive",
+    #     "tests/unit/examples/test_notebooks_gpu.py::test_fastai",
+    # ],
+    # "group_notebooks_gpu_002": [  # Total group time: 241.15s
+    #     "tests/unit/examples/test_notebooks_gpu.py::test_gpu_vm",  # 0.76s (Always the first test to check the GPU works)
+    #     "tests/unit/examples/test_notebooks_gpu.py::test_wide_deep",
+    #     "tests/unit/examples/test_notebooks_gpu.py::test_xdeepfm",
+    #     "tests/unit/examples/test_notebooks_gpu.py::test_gpu_vm",
+    # ],
+    # "group_cpu_001": [  # Total group time: 525.96s
+    #     "tests/unit/recommenders/datasets/test_movielens.py::test_load_pandas_df_mock_100__with_default_param__succeed",
+    #     "tests/unit/recommenders/datasets/test_dataset.py::test_maybe_download_wrong_bytes",
+    #     "tests/unit/recommenders/datasets/test_movielens.py::test_mock_movielens_schema__has_default_col_names",
+    #     "tests/unit/recommenders/datasets/test_movielens.py::test_load_pandas_df_mock_100__with_custom_param__succeed",
+    #     "tests/unit/recommenders/datasets/test_dataset.py::test_maybe_download_retry",
+    #     "tests/unit/recommenders/datasets/test_movielens.py::test_mock_movielens_schema__get_df__return_success",
+    #     "tests/unit/recommenders/utils/test_timer.py::test_timer",
+    #     "tests/unit/recommenders/tuning/test_ncf_utils.py::test_compute_test_results__return_success",
+    #     "tests/unit/recommenders/datasets/test_movielens.py::test_mock_movielens_schema__get_df_remove_default_col__return_success",
+    #     "tests/unit/recommenders/models/test_geoimc.py::test_imcproblem",
+    #     "tests/unit/recommenders/datasets/test_wikidata.py::test_find_wikidata_id",
+    #     "tests/unit/recommenders/models/test_sar_singlenode.py::test_sar_item_similarity",
+    #     "tests/unit/recommenders/models/test_tfidf_utils.py::test_tokenize_text",
+    #     "tests/unit/recommenders/models/test_tfidf_utils.py::test_get_tokens",
+    #     "tests/unit/recommenders/models/test_cornac_utils.py::test_recommend_k_items",
+    #     "tests/unit/recommenders/evaluation/test_python_evaluation_time_performance.py",  # 297.91s
+    # ],
+    # "group_notebooks_cpu_001": [  # Total group time: 226.42s
+    #     "tests/unit/examples/test_notebooks_python.py::test_rlrmc_quickstart_runs",
+    #     "tests/unit/examples/test_notebooks_python.py::test_sar_deep_dive_runs",
+    #     "tests/unit/examples/test_notebooks_python.py::test_baseline_deep_dive_runs",
+    #     "tests/unit/examples/test_notebooks_python.py::test_template_runs",
+    #     "tests/unit/recommenders/utils/test_notebook_utils.py::test_is_jupyter",
+    #     "tests/unit/examples/test_notebooks_python.py::test_surprise_deep_dive_runs",
+    #     "tests/unit/examples/test_notebooks_python.py::test_lightgbm",
+    #     "tests/unit/examples/test_notebooks_python.py::test_cornac_deep_dive_runs",
+    #     "tests/unit/examples/test_notebooks_python.py::test_sar_single_node_runs",
+    # ],
 }
