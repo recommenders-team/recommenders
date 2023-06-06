ffac856156996011328841dff8fb292bc5de053f
Simon Zhao <43029286+simonzhaoms@users.noreply.github.com>
2023-06-06 19:49:59 +0800

    Add support for Python 3.10 and 3.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import site
 import sys
 import time
 
-# workround for enabling editable user pip installs
+# workaround for enabling editable user pip installs
 site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 # version
@@ -27,61 +27,57 @@ if HASH is not None:
     version += ".post" + str(int(time.time()))
 
 install_requires = [
-    "numpy>=1.19",  # 1.19 required by tensorflow 2.6
-    "pandas>1.0.3,<2",
-    "scipy>=1.0.0,<2",
-    "tqdm>=4.31.1,<5",
-    "matplotlib>=2.2.2,<4",
-    "scikit-learn>=0.22.1,<1.0.3",
-    "numba>=0.38.1,<1",
-    "lightfm>=1.15,<2",
-    "lightgbm>=2.2.1",
-    "memory_profiler>=0.54.0,<1",
-    "nltk>=3.4,<4",
-    "seaborn>=0.8.1,<1",
-    "transformers>=2.5.0,<5",
-    "bottleneck>=1.2.1,<2",
-    "category_encoders>=1.3.0,<2",
-    "jinja2>=2,<3.1",
-    "pyyaml>=5.4.1,<6",
-    "requests>=2.0.0,<3",
-    "cornac>=1.1.2,<2",
-    "retrying>=1.3.3",
-    "pandera[strategies]>=0.6.5",  # For generating fake datasets
-    "scikit-surprise>=1.0.6",
+    "pandas>1.5.2,<2.1",  # requires numpy
+    "tqdm>=4.65.0,<5",
+    "matplotlib>=3.6.0,<4",
+    "scikit-learn>=1.1.3,<2",  # 1.0.2 may not support Python 3.10.  requires scipy
+    "numba>=0.57.0,<1",
+    "lightfm>=1.17,<2",
+    "lightgbm>=3.3.2,<4",
+    "memory_profiler>=0.61.0,<1",
+    "nltk>=3.8.1,<4",
+    "seaborn>=0.12.0,<1",
+    "transformers>=4.26.0,<5",  # requires pyyaml
+    "bottleneck>=1.3.7,<2",
+    "category_encoders>=2.6.0,<2",
+    "jinja2>=3.1.0,<3.2",
+    "cornac>=1.15.2,<2",
+    "retrying>=1.3.4",
+    "pandera[strategies]>=0.15.0",  # For generating fake datasets
+    "scikit-surprise>=1.1.3",
     "scrapbook>=0.5.0,<1.0.0",
 ]
 
 # shared dependencies
 extras_require = {
     "examples": [
-        "azure.mgmt.cosmosdb>=0.8.0,<1",
-        "hyperopt>=0.1.2,<1",
-        "ipykernel>=4.6.1,<7",
-        "jupyter>=1,<2",
-        "locust>=1,<2",
-        "papermill>=2.1.2,<3",
+        "azure-mgmt-cosmosdb>=9.0.0,<10",
+        "hyperopt>=0.2.7,<1",
+        "ipykernel>=6.20.1,<7",
+        "notebook>=6.5.4,<8",
+        "locust>=2.15.1,<3",
+        "papermill>=2.4.0,<3",
     ],
     "gpu": [
-        "nvidia-ml-py3>=7.352.0",
+        "nvidia-ml-py3>=11.510.69",
         # TensorFlow compiled with CUDA 11.2, cudnn 8.1
         "tensorflow~=2.6.1;python_version=='3.6'",
         "tensorflow~=2.7.0;python_version>='3.7'",
         "tf-slim>=1.1.0",
-        "torch>=1.8",  # for CUDA 11 support
-        "fastai>=1.0.46,<2",
+        "torch>=2.0.1",
+        "fastai>=2.7.11,<3",
     ],
     "spark": [
-        "databricks_cli>=0.8.6,<1",
-        "pyarrow>=0.12.1,<7.0.0",
-        "pyspark>=2.4.5,<3.3.0",
+        "databricks_cli>=0.17.7,<1",
+        "pyarrow>=10.0.1",
+        "pyspark>=3.0.1,<=3.4.0",
     ],
     "dev": [
-        "black>=18.6b4,<21",
-        "pytest>=3.6.4",
-        "pytest-cov>=2.12.1",
-        "pytest-mock>=3.6.1",  # for access to mock fixtures in pytest
-        "pytest-rerunfailures>=10.2",  # to mark flaky tests
+        "black>=23.3.0,<24",
+        "pytest>=7.2.1",
+        "pytest-cov>=4.1.0",
+        "pytest-mock>=3.10.0",  # for access to mock fixtures in pytest
+        "pytest-rerunfailures>=11.1.2",  # to mark flaky tests
     ],
 }
 # for the brave of heart
@@ -124,9 +120,10 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
@@ -136,7 +133,7 @@ setup(
     "machine learning python spark gpu",
     install_requires=install_requires,
     package_dir={"recommenders": "recommenders"},
-    python_requires=">=3.6, <3.10",
+    python_requires=">=3.8, <3.12",
     packages=find_packages(where=".", exclude=["contrib", "docs", "examples", "scenarios", "tests", "tools"]),
     setup_requires=["numpy>=1.15"]
 )
