03e130c2563750b742df4a30c32efe0520f06fa7
miguelgfierro <miguelgfierro@users.noreply.github.com>
2023-06-29 13:11:37 +0200

    Remove scipy 1.11.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if HASH is not None:
 install_requires = [
     "numpy>=1.19",  # 1.19 required by tensorflow 2.6
     "pandas>1.0.3,<2",
-    "scipy>=1.0.0,<2",
+    "scipy>=1.0.0,<2,!=1.11.0",
     "tqdm>=4.31.1,<5",
     "matplotlib>=2.2.2,<4",
     "scikit-learn>=0.22.1,<1.0.3",
@@ -44,7 +44,7 @@ install_requires = [
     "jinja2>=2,<3.1",
     "requests>=2.0.0,<3",
     "cornac>=1.1.2,<1.15.2;python_version<='3.7'",
-    "cornac>=1.15.2,<2;python_version>='3.8'", # After 1.15.2, Cornac requires python 3.8
+    "cornac>=1.15.2,<2;python_version>='3.8'",  # After 1.15.2, Cornac requires python 3.8
     "retrying>=1.3.3",
     "pandera[strategies]>=0.6.5",  # For generating fake datasets
     "scikit-surprise>=1.0.6",
