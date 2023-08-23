a6bff6e259da4a081811e96af32b728f476a7aa3
miguelgfierro <miguelgfierro@users.noreply.github.com>
2023-08-23 10:40:37 +0200

    Remove MacOS from supported OS
    Signed-off-by: miguelgfierro <miguelgfierro@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -124,7 +124,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Operating System :: POSIX :: Linux",
-        "Operating System :: MacOS",
     ],
     extras_require=extras_require,
     keywords="recommendations recommendation recommenders recommender system engine "
