9364c9b7305fa0a8073816cee3c4a157afe94b65
Simon Zhao <simonyansenzhao@gmail.com>
2023-09-04 14:52:43 +0800

    Add hypothesis<6.83.1

--- setup.py
+++ setup.py
@@ -46,6 +46,9 @@ install_requires = [
     "hyperopt>=0.2.7,<1",
     "notebook>=7.0.0,<8",  # requires jupyter, ipykernel
     "locust>=2.12.2,<3",
+    # 6.83.1 introduced a non-existent attribute '_deferred_pprinters' of IPython.lib.pretty in
+    # https://github.com/HypothesisWorks/hypothesis/commit/5ea8e0c3e6da1cd9fb3f302124dc74791c14db11
+    "hypothesis<6.83.1",
 ]
 
 # shared dependencies
