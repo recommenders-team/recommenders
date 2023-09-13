1371084c7590d3e2ea88e0ad1071b879d7853d33
Simon Zhao <simonyansenzhao@gmail.com>
2023-09-13 17:42:10 +0800

    Revert dependency versions
    Signed-off-by: Simon Zhao <simonyansenzhao@gmail.com>

--- setup.py
+++ setup.py
@@ -28,46 +28,51 @@ if HASH is not None:
 
 install_requires = [
     "numpy>=1.19",  # 1.19 required by tensorflow 2.6
-    "pandas>1.0.3,<2",  # requires numpy
+    "pandas>1.0.3,<2",
     "scipy>=1.0.0,<1.11.0",  # FIXME: We limit <1.11.0 until #1954 is fixed
-    "scikit-learn>=0.22.1,<1.4",  # requires scipy
+    "tqdm>=4.31.1,<5",
+    "matplotlib>=2.2.2,<4",
+    "scikit-learn>=0.22.1,<1.0.3",
     "numba>=0.38.1,<1",
     "lightfm>=1.15,<2",
     "lightgbm>=2.2.1",
-    "memory-profiler>=0.54.0,<1",
-    "nltk>=3.4,<4",  # requires tqdm
-    "seaborn>=0.8.1,<1",  # requires matplotlib
-    "transformers>=4.0.0,<5",  # requires pyyaml, tqdm
-    "category-encoders>=1.3.0,<3",
-    "jinja2>=2,<3.2",
-    "cornac>=1.1.2,<1.15.2;python_version<='3.7'",  # requires tqdm
+    "memory_profiler>=0.54.0,<1",
+    "nltk>=3.4,<4",
+    "seaborn>=0.8.1,<1",
+    "transformers>=2.5.0,<5",
+    "category_encoders>=1.3.0,<2",
+    "jinja2>=2,<3.1",
+    "requests>=2.0.0,<3",
+    "cornac>=1.1.2,<1.15.2;python_version<='3.7'",
     "cornac>=1.15.2,<2;python_version>='3.8'",  # After 1.15.2, Cornac requires python 3.8
     "retrying>=1.3.3",
     "pandera[strategies]>=0.6.5",  # For generating fake datasets
     "scikit-surprise>=1.0.6",
-    "scrapbook>=0.5.0,<1.0.0",  # requires tqdm, papermill
+    "scrapbook>=0.5.0,<1.0.0",
     "hyperopt>=0.1.2,<1",
-    "notebook>=5.0.0,<8",  # requires jupyter, ipykernel
-    "locust>=1,<3",
+    "ipykernel>=4.6.1,<7",
+    "jupyter>=1,<2",
+    "locust>=1,<2",
+    "papermill>=2.1.2,<3",
 ]
 
 # shared dependencies
 extras_require = {
     "gpu": [
-        "nvidia-ml-py>=7.352.0",
+        "nvidia-ml-py3>=7.352.0",
         # TensorFlow compiled with CUDA 11.2, cudnn 8.1
         "tensorflow~=2.6.1;python_version=='3.6'",
-        "tensorflow>=2.7.0;python_version>='3.7'",
+        "tensorflow~=2.7.0;python_version>='3.7'",
         "tf-slim>=1.1.0",
         "torch>=1.8",  # for CUDA 11 support
         "fastai>=1.0.46,<2",
     ],
     "spark": [
-        "pyarrow>=0.12.1,<14.0.0",
-        "pyspark>=2.4.5,<3.5",
+        "pyarrow>=0.12.1,<7.0.0",
+        "pyspark>=2.4.5,<3.3.0",
     ],
     "dev": [
-        "black>=18.6b4,<24",
+        "black>=18.6b4,<21",
         "pytest>=3.6.4",
         "pytest-cov>=2.12.1",
         "pytest-mock>=3.6.1",  # for access to mock fixtures in pytest
