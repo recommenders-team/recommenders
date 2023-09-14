659ac17710435e62dac05585c63ec01a76082f23
Simon Zhao <simonyansenzhao@gmail.com>
2023-09-14 10:04:42 +0800

    Update
    Signed-off-by: Simon Zhao <simonyansenzhao@gmail.com>

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ setup(
     "machine learning python spark gpu",
     install_requires=install_requires,
     package_dir={"recommenders": "recommenders"},
-    python_requires=">=3.6, <=3.9",
+    python_requires=">=3.6, <3.10",
     packages=find_packages(
         where=".",
         exclude=["contrib", "docs", "examples", "scenarios", "tests", "tools"],
