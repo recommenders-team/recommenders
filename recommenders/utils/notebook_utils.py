bda7322e9b67bfcd8f07299a2a670b19b0adc555
miguelgfierro <miguelgfierro@users.noreply.github.com>
2023-12-23 09:58:25 +0100

    :memo:
    Signed-off-by: miguelgfierro <miguelgfierro@users.noreply.github.com>
    Signed-off-by: Simon Zhao <simonyansenzhao@gmail.com>

--- recommenders/utils/notebook_utils.py
+++ recommenders/utils/notebook_utils.py
@@ -114,7 +114,6 @@ def execute_notebook(
 
 def store_metadata(name, value):
     """Store data in the notebook's output source code.
-    This function is similar to snapbook.glue().
 
     Args:
         name (str): Name of the data.
@@ -134,7 +133,6 @@ def store_metadata(name, value):
 
 def read_notebook(path):
     """Read the metadata stored in the notebook's output source code.
-    This function is similar to snapbook.read_notebook().
 
     Args:
         path (str): Path to the notebook.
