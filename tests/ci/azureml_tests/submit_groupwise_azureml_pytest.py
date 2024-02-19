530f85b9d240b8d6577459f0f09469cac86bec30
Simon Zhao <simonyansenzhao@gmail.com>
2024-02-19 20:57:42 +0800

    Remove leading whitespaces in Dockerfile
    Signed-off-by: Simon Zhao <simonyansenzhao@gmail.com>

--- tests/ci/azureml_tests/submit_groupwise_azureml_pytest.py
+++ tests/ci/azureml_tests/submit_groupwise_azureml_pytest.py
@@ -186,28 +186,25 @@ def create_run_config(
         # * https://learn.microsoft.com/en-us/azure/machine-learning/how-to-train-with-custom-image?view=azureml-api-1#use-a-custom-dockerfile-optional
         # * https://github.com/Azure/AzureML-Containers/blob/master/base/gpu/openmpi4.1.0-cuda11.8-cudnn8-ubuntu22.04
         run_azuremlcompute.environment.docker.base_dockerfile = r"""
-            FROM nvcr.io/nvidia/cuda:12.3.1-devel-ubuntu22.04
-
-            USER root:root
-
-            ENV NVIDIA_VISIBLE_DEVICES all
-            ENV NVIDIA_DRIVER_CAPABILITIES compute,utility
-            ENV LANG=C.UTF-8 LC_ALL=C.UTF-8
-            ENV DEBIAN_FRONTEND noninteractive
-            
-            # Conda Environment
-            ENV MINICONDA_VERSION py38_23.3.1-0
-            ENV PATH /opt/miniconda/bin:$PATH
-            ENV CONDA_PACKAGE 23.5.0
-            RUN wget -qO /tmp/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh && \
-                bash /tmp/miniconda.sh -bf -p /opt/miniconda && \
-                conda install conda=${CONDA_PACKAGE} -y && \
-                conda update --all -c conda-forge -y && \
-                conda clean -ay && \
-                rm -rf /opt/miniconda/pkgs && \
-                rm /tmp/miniconda.sh && \
-                find / -type d -name __pycache__ | xargs rm -rf
-        """
+FROM nvcr.io/nvidia/cuda:12.3.1-devel-ubuntu22.04
+USER root:root
+ENV NVIDIA_VISIBLE_DEVICES all
+ENV NVIDIA_DRIVER_CAPABILITIES compute,utility
+ENV LANG=C.UTF-8 LC_ALL=C.UTF-8
+ENV DEBIAN_FRONTEND noninteractive
+# Conda Environment
+ENV MINICONDA_VERSION py38_23.3.1-0
+ENV PATH /opt/miniconda/bin:$PATH
+ENV CONDA_PACKAGE 23.5.0
+RUN wget -qO /tmp/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh && \
+    bash /tmp/miniconda.sh -bf -p /opt/miniconda && \
+    conda install conda=${CONDA_PACKAGE} -y && \
+    conda update --all -c conda-forge -y && \
+    conda clean -ay && \
+    rm -rf /opt/miniconda/pkgs && \
+    rm /tmp/miniconda.sh && \
+    find / -type d -name __pycache__ | xargs rm -rf
+"""
 
     # Use conda_dependencies.yml to create a conda environment in
     # the Docker image for execution
