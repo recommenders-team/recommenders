15fbf90eb993b17f592d8c980dece4de1bbdb2ad
Simon Zhao <simonyansenzhao@gmail.com>
2024-02-22 11:37:49 +0800

    Pin pip=20.1.1

--- tests/ci/azureml_tests/submit_groupwise_azureml_pytest.py
+++ tests/ci/azureml_tests/submit_groupwise_azureml_pytest.py
@@ -197,12 +197,14 @@ RUN apt-get update && \
     apt-get clean -y && \
     rm -rf /var/lib/apt/lists/*
 # Conda Environment
+# Pin pip=20.1.1 due to the issue: No module named 'ruamel'
+# See https://learn.microsoft.com/en-us/python/api/overview/azure/ml/install?view=azure-ml-py#troubleshooting
 ENV MINICONDA_VERSION py38_23.3.1-0
 ENV PATH /opt/miniconda/bin:$PATH
 ENV CONDA_PACKAGE 23.5.0
 RUN wget -qO /tmp/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh && \
     bash /tmp/miniconda.sh -bf -p /opt/miniconda && \
-    conda install conda=${CONDA_PACKAGE} -y && \
+    conda install -y conda=${CONDA_PACKAGE} python=3.8 pip=20.1.1 && \
     conda update --all -c conda-forge -y && \
     conda clean -ay && \
     rm -rf /opt/miniconda/pkgs && \
