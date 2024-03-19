b255fae97203ee0f82bbc5faba1f971d07a25f80
miguelgfierro <miguelgfierro@users.noreply.github.com>
2024-03-19 07:14:39 +0100

    :memo:
    Signed-off-by: miguelgfierro <miguelgfierro@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ extras_require = {
     "gpu": [
         "fastai>=2.7.11,<3",
         "nvidia-ml-py>=11.525.84",
-        "tensorflow>=2.8.4,!=2.9.0.*,!=2.9.1,!=2.9.2,!=2.10.0.*,<2.16",
+        "tensorflow>=2.8.4,!=2.9.0.*,!=2.9.1,!=2.9.2,!=2.10.0.*,<2.16",  # Fixed TF due to constant security problems and breaking changes #2073
         "tf-slim>=1.1.0",  # No python_requires in its setup.py
         "torch>=2.0.1,<3",
     ],
