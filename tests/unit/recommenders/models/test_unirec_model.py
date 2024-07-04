17b67b71d96a08e5e872a00c6b58492757a99b4f
miguelgfierro <miguelgfierro@users.noreply.github.com>
2024-07-04 13:22:36 +0200

    seqbase
    Signed-off-by: miguelgfierro <miguelgfierro@users.noreply.github.com>

--- tests/unit/recommenders/models/test_unirec_model.py
+++ tests/unit/recommenders/models/test_unirec_model.py
@@ -7,6 +7,8 @@ import pytest
 
 try:
     from recommenders.models.unirec.utils.file_io import load_yaml
+    from recommenders.models.unirec.model.base.reco_abc import AbstractRecommender
+    from recommenders.models.unirec.model.sequential.seqrec_base import SeqRecBase
     from recommenders.models.unirec.model.sequential.sasrec import SASRec
 except ImportError:
     pass  # skip this import if we are in cpu environment
@@ -23,6 +25,34 @@ def base_config(unirec_config_path):
     config["device"] = "cuda"
 
     return config
+
+
+@pytest.mark.gpu
+def test_seqrecbase_component_definition(base_config):
+    model = SeqRecBase(base_config)
+
+    assert model.config["exp_name"] == "pytest"
+    assert model.n_users == 1
+    assert model.n_items == 1
+    assert model.device == "cuda"
+    assert model.loss_type == "bce"
+    assert model.embedding_size == 32
+    assert (
+        model.hidden_size == 32
+    )  # FIXME: Review https://github.com/microsoft/UniRec/pulls/12
+    assert model.dropout_prob == 0.0
+    assert model.use_pre_item_emb == 0
+    assert model.use_text_emb == 0
+    assert model.text_emb_size == 768
+    assert model.init_method == "normal"
+    assert model.use_features == 0
+    assert model.group_size == -1
+    assert model.SCORE_CLIP == -1
+    assert model.has_user_bias is False
+    assert model.has_item_bias is False
+    assert model.tau == 1.0
+
+    assert model.annotations == ["AbstractRecommender", "SeqRecBase"]
 
 
 @pytest.mark.gpu
