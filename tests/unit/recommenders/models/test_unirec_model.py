e9e6733391ff0ea2d6cfeb2ae703cc12314924bd
miguelgfierro <miguelgfierro@users.noreply.github.com>
2024-07-13 08:43:28 +0200

    WIP
    Signed-off-by: miguelgfierro <miguelgfierro@users.noreply.github.com>

--- tests/unit/recommenders/models/test_unirec_model.py
+++ tests/unit/recommenders/models/test_unirec_model.py
@@ -5,6 +5,7 @@ import os
 import copy
 import pytest
 
+
 try:
     import torch
     import torch.nn as nn
@@ -86,3 +87,62 @@ def test_sasrec_component_definition(base_config, unirec_config_path):
     assert isinstance(model.trm_encoder, nn.Module) is True
     assert isinstance(model.LayerNorm, nn.LayerNorm) is True
     assert isinstance(model.dropout, nn.Dropout) is True
+
+
+@pytest.mark.gpu
+def test_sasrec_train(base_config, unirec_config_path):
+    # config = copy.deepcopy(base_config)
+    # yaml_file = os.path.join(unirec_config_path, "model", "SASRec.yaml")
+    # config.update(load_yaml(yaml_file))
+
+    # model = SASRec(config)
+    import copy
+    import datetime
+    from recommenders.models.unirec.main import main
+
+    GLOBAL_CONF = {
+        # "config_dir": f"{os.path.join(unirec_config_path, 'unirec', 'config')}",
+        "config_dir": unirec_config_path,
+        "exp_name": "pytest",
+        "checkpoint_dir": f'{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}',
+        "model": "",
+        "dataloader": "SeqRecDataset",
+        "dataset": "",
+        "dataset_path": os.path.join(unirec_config_path, "tests/.temp/data"),
+        "output_path": "",
+        "learning_rate": 0.001,
+        "dropout_prob": 0.0,
+        "embedding_size": 32,
+        "hidden_size": 32,
+        "use_pre_item_emb": 0,
+        "loss_type": "bce",
+        "max_seq_len": 10,
+        "has_user_bias": 1,
+        "has_item_bias": 1,
+        "epochs": 1,
+        "early_stop": -1,
+        "batch_size": 512,
+        "n_sample_neg_train": 9,
+        "valid_protocol": "one_vs_all",
+        "test_protocol": "one_vs_all",
+        "grad_clip_value": 0.1,
+        "weight_decay": 1e-6,
+        "history_mask_mode": "autoagressive",
+        "user_history_filename": "user_history",
+        "metrics": "['hit@5;10', 'ndcg@5;10']",
+        "key_metric": "ndcg@5",
+        "num_workers": 4,
+        "num_workers_test": 0,
+        "verbose": 2,
+        "neg_by_pop_alpha": 0.0,
+        "conv_size": 10,  # for ConvFormer-series
+    }
+    config = copy.deepcopy(GLOBAL_CONF)
+    config["task"] = "train"
+    config["dataset_path"] = os.path.join(config["dataset_path"], "ml-100k")
+    config["dataset"] = "ml-100k"
+    config["model"] = "SASRec"
+    config["output_path"] = os.path.join(unirec_config_path, f"tests/.temp/output/")
+    result = main.run(config)
+    assert result is not None
+    print(result)
