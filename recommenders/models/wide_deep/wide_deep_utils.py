fc49b135db85315e705f29867aa80b099a5eadfd
David Davó <david@ddavo.me>
2024-09-20 15:43:29 +0000

    Added support for additional embeddings in wide and deep pytorch module
    Signed-off-by: David Davó <david@ddavo.me>

--- recommenders/models/wide_deep/wide_deep_utils.py
+++ recommenders/models/wide_deep/wide_deep_utils.py
@@ -1,7 +1,7 @@
 # Copyright (c) Recommenders contributors.
 # Licensed under the MIT License.
-from typing import Tuple
-from dataclasses import dataclass
+from typing import Tuple, Dict, Optional
+from dataclasses import dataclass, field
 
 import torch
 from torch.utils.data import DataLoader
@@ -15,6 +15,8 @@ class WideAndDeepHyperParams:
     item_dim: int = 32
     dnn_hidden_units: Tuple[int, ...] = (128, 128)
     dnn_dropout: float = 0.0
+    dnn_additional_embeddings_sizes: dict[str, Tuple[int, int]] = field(default_factory=dict)
+    dnn_cont_features: int = 0
 
 class WideAndDeepModel(nn.Module):
     def __init__(
@@ -29,22 +31,20 @@ class WideAndDeepModel(nn.Module):
         self.n_users = num_users
         self.n_items = num_items
         
-        self.embeddings = nn.ModuleDict({
-            'users_emb': nn.Embedding(num_users, hparams.user_dim),
-            'items_emb': nn.Embedding(num_items, hparams.item_dim),
-            # TODO: Put other embeddings for other categorical features
+        self.users_emb = nn.Embedding(num_users, hparams.user_dim)
+        self.items_emb = nn.Embedding(num_items, hparams.item_dim)
+        self.additional_embs = nn.ModuleDict({
+            k: nn.Embedding(num, dim) for k, (num, dim) in hparams.dnn_additional_embeddings_sizes.items()
         })
 
         # Randomly initialize embeddings
-        total_emb_dim = 0
-        for _, emb in self.embeddings.items():
+        total_emb_dim = hparams.user_dim + hparams.item_dim
+        for _, emb in self.additional_embs.items():
             total_emb_dim += emb.embedding_dim
             nn.init.uniform_(emb.weight, -1, 1)
 
         layers = []
-        # TODO: Add support for continuous features
-        cont_features = 0
-        prev_output = cont_features + total_emb_dim
+        prev_output = hparams.dnn_cont_features + total_emb_dim
         for hu in hparams.dnn_hidden_units:
             layers.append(nn.Linear(prev_output, hu))
             layers.append(nn.Dropout(hparams.dnn_dropout))
@@ -65,25 +65,33 @@ class WideAndDeepModel(nn.Module):
             nn.Sigmoid(),
         )
 
-    def forward(self, interactions: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self, 
+        interactions: torch.Tensor, 
+        additional_embeddings: Dict[str, torch.Tensor] = {},
+        continuous_features: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
         users, items = interactions.T
         all_embed = torch.cat([
-            self.embeddings['users_emb'](users), # Receives the indices
-            self.embeddings['items_emb'](items),
+            self.users_emb(users), # Receives the indices
+            self.items_emb(items),
+            *[ emb(additional_embeddings[k]) for k, emb in self.additional_embs.items() ]
         ], dim=1)
 
         # The cross-feature is really only the items because there is no
-        # impression data
+        # impression data??
         # https://datascience.stackexchange.com/a/58915/169220
         cross_product = torch.zeros([items.numel(), self.n_items])
         cross_product[torch.arange(items.numel()), items] = 1
 
-        # TODO: Add support for continuous features (concat inputs)
-        deep_output = self.deep(all_embed)
+        if self.hparams.dnn_cont_features > 0:
+            deep_input = torch.cat([continuous_features, all_embed], dim=1)
+        else:
+            deep_input = all_embed
 
         return self.head(torch.cat([
             cross_product, # wide input
-            deep_output, # deep output
+            self.deep(deep_input), # deep output
         ], dim=1))
 
 
