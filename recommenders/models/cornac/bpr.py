f4d508d02c2ca7d3d9f0bb4c67049ec11acd605d
miguelgfierro <miguelgfierro@users.noreply.github.com>
2025-07-20 09:23:43 +0200

    opt
    Signed-off-by: miguelgfierro <miguelgfierro@users.noreply.github.com>

--- recommenders/models/cornac/bpr.py
+++ recommenders/models/cornac/bpr.py
@@ -1,7 +1,6 @@
 # Copyright (c) Recommenders contributors.
 # Licensed under the MIT License.
 
-
 import numpy as np
 import pandas as pd
 from cornac.models import BPR as CBPR
@@ -11,7 +10,6 @@ from recommenders.utils.constants import (
     DEFAULT_ITEM_COL,
     DEFAULT_RATING_COL,
 )
-
 
 class BPR(CBPR):
     """Custom BPR class extending Cornac's BPR model with a recommend_k_items method."""
@@ -28,40 +26,50 @@ class BPR(CBPR):
         col_item=DEFAULT_ITEM_COL,
         col_rating=DEFAULT_RATING_COL,
     ):
-        """Computes predictions of recommender model from Cornac on all users and items in data.
-            It can be used for computing ranking metrics like NDCG.
+        """Computes top-k predictions of recommender model from Cornac on all users in data.
+        It can be used for computing ranking metrics like NDCG.
 
         Args:
-            data (pandas.DataFrame): The data from which to get the users and items
-            usercol (str): Name of the user column
-            itemcol (str): Name of the item column
-            predcol (str): Name of the prediction column
-            remove_seen (bool): Flag to remove (user, item) pairs seen in the training data
+            data (pandas.DataFrame): The data from which to get the users and items.
+            top_k (int): Number of items to recommend per user.
+            remove_seen (bool): Flag to remove (user, item) pairs seen in the training data.
+            col_user (str): Name of the user column.
+            col_item (str): Name of the item column.
+            col_rating (str): Name of the prediction column.
 
         Returns:
-            pandas.DataFrame: Dataframe with usercol, itemcol, predcol
+            pandas.DataFrame: Dataframe with col_user, col_item, col_rating for top-k items per user.
         """
-        # Precompute items and users
-        items = list(self.train_set.iid_map.keys())
-        users = list(self.train_set.uid_map.keys())
+        # Get user and item mappings
+        items = np.array(list(self.train_set.iid_map.keys()))
+        users = np.array(list(self.train_set.uid_map.keys()))
         n_users = len(users)
         n_items = len(items)
 
-        # Compute full score matrix in one go
-        user_indices = [self.train_set.uid_map[u] for u in users]
-        item_indices = [self.train_set.iid_map[i] for i in items]
+        # Compute user and item indices
+        user_indices = np.array([self.train_set.uid_map[u] for u in users])
+        item_indices = np.array([self.train_set.iid_map[i] for i in items])
+
+        # Get latent factors and biases
         U = self.u_factors[user_indices]
         V = self.i_factors[item_indices]
-        B = self.i_biases[item_indices]
+        B = self.i_biases[item_indices] if hasattr(self, 'i_biases') else np.zeros(n_items)
 
-        # Matrix multiplication for all user-item pairs
-        preds_matrix = U @ V.T + B
-        user_array = np.repeat(users, n_items)
-        item_array = np.tile(items, n_users)
-        preds = preds_matrix.flatten()
+        # Compute score matrix for all user-item pairs
+        preds_matrix = U @ V.T + B  # Shape: (n_users, n_items)
 
+        # Select top-k items per user
+        top_k_indices = np.argpartition(preds_matrix, -top_k, axis=1)[:, -top_k:]  # Partial sort
+        top_k_indices = top_k_indices[:, np.argsort(-preds_matrix[np.arange(n_users)[:, None], top_k_indices])]  # Sort top-k
+
+        # Flatten arrays for DataFrame
+        user_array = np.repeat(users, top_k)
+        item_array = items[top_k_indices].flatten()
+        pred_array = preds_matrix[np.arange(n_users)[:, None], top_k_indices].flatten()
+
+        # Create DataFrame
         all_predictions = pd.DataFrame(
-            {col_user: user_array, col_item: item_array, col_rating: preds}
+            {col_user: user_array, col_item: item_array, col_rating: pred_array}
         )
 
         if remove_seen:
@@ -69,5 +77,5 @@ class BPR(CBPR):
             merged = all_predictions.merge(
                 seen, on=[col_user, col_item], how="left", indicator=True
             )
-            return merged[merged["_merge"] == "left_only"].drop(columns=["_merge"])
+            return merged[merged["_merge"] == "left_only"].drop(columns=["_merge"]).reset_index(drop=True)
         return all_predictions
