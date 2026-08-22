6514b925542bcee47c406f8a47b97f3db93f6f06
Simon Zhao <simonyansenzhao@gmail.com>
2026-08-22 13:03:44 +0800

    Add setup_vm.sh in CompShare tools
    Signed-off-by: Simon Zhao <simonyansenzhao@gmail.com>

---
+++ .github/workflows/tools/compshare/setup_vm.sh
@@ -0,0 +1,59 @@
+#! /bin/bash -
+
+# Copyright (c) Recommenders contributors.
+# Licensed under the MIT License.
+
+######################################################################
+# Run create_vm.sh to set up a CompShare VM.
+# 
+# Params:
+# * VM name
+# * Test type
+#
+# The following environment variables must be set:
+# * COMPSHARE_PRIVATE_KEY
+# * COMPSHARE_PUBLIC_KEY
+#
+# The following environment variables may need to be set:
+# * VM_DOCKER_MIRROR_URL
+# * VM_HTTP_PROXY
+# * VM_HTTPS_PROXY
+# * VM_PROXY_CERTIFICATE
+######################################################################
+set -euo pipefail
+shopt -s inherit_errexit
+
+script_dir="$(dirname "$0")"
+vm_name="${1:-}"
+test_type="${2:-}"
+[[ -z "${vm_name}" || -z "${test_type}" ]] && exit 1
+
+# Utility functions
+script_utils="${script_dir}/utils.sh"
+script_create="${script_dir}/create_vm.sh"
+
+# All VMs from CompShare have GPUs.
+# Nightly tests require more time and more GPU memory.
+# VMs with Spot ChargeType are cheaper but there is a risk of being
+# deleted after 1 hour.
+if [[ "${test_type}" == *nightly* ]]; then
+    gpu_type='"GPUType": "!2080,P40"'
+    gpu_mem='"Memory": {"GPU": 12, "CPU": 32}'
+    charge_type='"ChargeType": ["Postpay"]'
+    stop_time="\"SchedulerStopTime\": $(date --date='3 hours' '+%s')"
+else
+    gpu_type='"GPUType": "!P40"'
+    gpu_mem='"Memory": {"GPU": 8, "CPU": 32}'
+    charge_type='"ChargeType": ["Spot","Postpay"]'
+    stop_time="\"SchedulerStopTime\": $(date --date='1 hours' '+%s')"
+fi
+requirements="{${gpu_type}, ${gpu_mem}, ${charge_type}, ${stop_time}}"
+bash "${script_create}" "${vm_name}" "${requirements}"
+
+unset COMPSHARE_PRIVATE_KEY
+
+echo 'Exporting VM info for subsequent steps ...'
+source "${script_utils}"
+mapfile -t vm_info < <(get_vm_info "${vm_name}")
+ssh_dest="${vm_info[1]}"
+echo "SSH_DEST=${ssh_dest}" >> "$GITHUB_ENV"
