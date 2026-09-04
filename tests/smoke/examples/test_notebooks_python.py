24d21bbd0c0957b98134c68e60da10d6503f3d0f
miguelgfierro <miguelgfierro@users.noreply.github.com>
2026-09-04 19:18:55 +0200

    Vectorize to_vw and simplify the candidate set construction
    Signed-off-by: miguelgfierro <miguelgfierro@users.noreply.github.com>

--- tests/smoke/examples/test_notebooks_python.py
+++ tests/smoke/examples/test_notebooks_python.py
@@ -86,7 +86,7 @@ def test_vw_deep_dive_smoke(notebooks, output_notebook, kernel_name):
     assert results["mae"] == pytest.approx(0.71292, rel=TOL, abs=ABS_TOL)
     assert results["rsquared"] == pytest.approx(0.231199, rel=TOL, abs=ABS_TOL)
     assert results["exp_var"] == pytest.approx(0.231337, rel=TOL, abs=ABS_TOL)
-    assert results["map"] == pytest.approx(0.041456, rel=TOL, abs=ABS_TOL)
+    assert results["map"] == pytest.approx(0.041627, rel=TOL, abs=ABS_TOL)
     assert results["ndcg"] == pytest.approx(0.096594, rel=TOL, abs=ABS_TOL)
     assert results["precision"] == pytest.approx(0.097770, rel=TOL, abs=ABS_TOL)
     assert results["recall"] == pytest.approx(0.037612, rel=TOL, abs=ABS_TOL)
